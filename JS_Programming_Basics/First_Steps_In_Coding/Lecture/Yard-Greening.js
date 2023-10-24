835cb44b82cc27d1854fb215aff4ed48a6204fda
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-10-24 21:27:25 +0300

    Create Yard-Greening.js

---
+++ JS_Programming_Basics/First_Steps_In_Coding/Lecture/Yard-Greening.js
@@ -0,0 +1,12 @@
+function calculate(input){
+    let pricePerSqrMeter = 7.61
+    let SqrMeters = Number (input[0])
+
+    let disscount = (pricePerSqrMeter * SqrMeters) * 0.18
+    let totalSum = (pricePerSqrMeter * SqrMeters) - disscount
+
+    console.log(`The final price is: ${totalSum} lv`)
+    console.log(`The discount is: ${disscount} lv.`)
+}
+calculate(["550"])
+calculate(["150"])
