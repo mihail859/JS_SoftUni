706bfd3bf82507f8b83c1f065614618ede062dcc
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-10-27 21:48:31 +0300

    Update 03_EvenOrOdd.js

--- JS_Programming_Basics/Conditional Statements - Lab/lecture/03_EvenOrOdd.js
+++ JS_Programming_Basics/Conditional Statements - Lab/lecture/03_EvenOrOdd.js
@@ -1,3 +1,13 @@
 function even_or_odd(input){
+    let number = Number (input[0])
 
+    if (number % 2 === 0){
+        console.log("even")
+    }
+    else{
+        console.log("odd")
+    }
 }
+
+even_or_odd(["2"])
+even_or_odd(["3"])
