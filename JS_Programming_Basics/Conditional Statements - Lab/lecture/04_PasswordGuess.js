1b5165305b5500004d979aba4dfb72dc894a5795
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-10-27 21:52:35 +0300

    Update 04_PasswordGuess.js

--- JS_Programming_Basics/Conditional Statements - Lab/lecture/04_PasswordGuess.js
+++ JS_Programming_Basics/Conditional Statements - Lab/lecture/04_PasswordGuess.js
@@ -1,3 +1,14 @@
 function guessPassword(input){
+    let password = "s3cr3t!P@ssw0rd"
+    let guess = input[0]
     
+    if (password === guess){
+        console.log("Welcome")
+    }
+    else{
+        console.log("Wrong password!")
+    }
 }
+
+guessPassword(["qwerty"])
+guessPassword(["s3cr3t!P@ssw0rd"])
