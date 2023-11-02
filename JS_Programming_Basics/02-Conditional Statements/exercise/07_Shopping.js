ddfa138291d6f00a6a2e00d7335c08123ba208a2
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-11-02 17:53:30 +0200

    update

--- JS_Programming_Basics/02-Conditional Statements/exercise/07_Shopping.js
+++ JS_Programming_Basics/02-Conditional Statements/exercise/07_Shopping.js
@@ -1,9 +1,42 @@
 function calculate(input){
-    /*1.	Бюджетът на Петър - реално число в интервала [0.0…100000.0]
-2.	Броят видеокарти - цяло число в интервала [0…100]
-3.	Броят процесори - цяло число в интервала [0…100]
-4.	Броят рам памет - цяло число в интервала [0…100]
-*/
+    
     let budget  = Number (input[0])
-    let videocards
+    let videocards = Number (input[1])
+    let processors = Number (input[2])
+    let ramMemory = Number (input[3])
+
+
+    let videocardsPrice = 250
+    let totalVideoCardsPrice = videocardsPrice * videocards
+
+    let processorsPrice = 0.35 * totalVideoCardsPrice
+    let ramMemoryPrice = 0.10 * totalVideoCardsPrice
+
+    let totalProcessorsPrice = processorsPrice * processors
+    let totalRamMemoryPrice = ramMemory * ramMemoryPrice
+
+    let totalBill = totalProcessorsPrice + totalRamMemoryPrice + totalVideoCardsPrice
+    if (videocards > processors){
+        totalBill *= 0.85
+    }
+
+    let difference = Math.abs(totalBill - budget)
+
+    if (budget >= totalBill){
+        console.log(`You have ${difference.toFixed(2)} leva left`)
+    }
+    else{
+        console.log(`Not enough money! You need ${difference.toFixed(2)} leva more!`)
+    }
+
 }
+
+calculate(["900",
+"2",
+"1",
+"3"])
+
+calculate(["920.45",
+"3",
+"1",
+"1"])
