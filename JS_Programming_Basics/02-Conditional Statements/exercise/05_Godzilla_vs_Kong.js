be0939c396a4f357614ed67d989c9d1f48e964a8
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-11-02 17:23:57 +0200

    Update 05_Godzilla_vs_Kong.js

--- JS_Programming_Basics/02-Conditional Statements/exercise/05_Godzilla_vs_Kong.js
+++ JS_Programming_Basics/02-Conditional Statements/exercise/05_Godzilla_vs_Kong.js
@@ -1,3 +1,29 @@
 function calculate(input){
     
+    let budgetFilm = Number (input[0])
+    let NumberOfStatists = Number (input[1])
+    let dressPrice = Number (input[2])
+
+    let decorPrice = 0.10 * budgetFilm
+    let dressPriceTotal = dressPrice * NumberOfStatists
+    if(NumberOfStatists > 150){
+        dressPriceTotal *= 0.90
+    }
+    let expenses = decorPrice + dressPriceTotal
+    let diff = Math.abs(expenses - budgetFilm)
+    if (expenses > budgetFilm){
+        console.log("Not enough money!")
+        console.log(`Wingard needs ${diff.toFixed(2)} leva more.`)
+    }
+    else{
+        console.log("Action!")
+        console.log(`Wingard starts filming with ${diff.toFixed(2)} leva left.`)
+    }
 }
+calculate(["20000",
+"120",
+"55.5"])
+
+calculate(["15437.62",
+"186",
+"57.99"])
