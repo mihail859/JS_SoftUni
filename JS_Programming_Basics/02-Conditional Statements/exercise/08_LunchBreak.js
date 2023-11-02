5cafb3b92302dd8c99a48ca9feefde5457069a37
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-11-02 20:49:09 +0200

    update

--- JS_Programming_Basics/02-Conditional Statements/exercise/08_LunchBreak.js
+++ JS_Programming_Basics/02-Conditional Statements/exercise/08_LunchBreak.js
@@ -1,3 +1,28 @@
 function breakCalculate(input){
     
+    let serialName = input[0]
+    let episodeDuration  = Number (input[1])
+    let breakDuration = Number (input[2])
+
+    let lunchTime = 1/8 * breakDuration
+    let freeTime = 1/4 * breakDuration
+
+    let leftTime = breakDuration - lunchTime - freeTime
+
+    let difference = Math.abs(leftTime - episodeDuration)
+
+    if (leftTime >= episodeDuration){
+        console.log(`You have enough time to watch ${serialName} and left with ${Math.ceil(difference)} minutes free time.`)
+    }
+    else{
+        console.log(`You don't have enough time to watch ${serialName}, you need ${Math.ceil(difference)} more minutes.`)
+    }
 }
+
+breakCalculate(["Game of Thrones",
+"60",
+"96"])
+
+breakCalculate(["Teen Wolf",
+"48",
+"60"])
