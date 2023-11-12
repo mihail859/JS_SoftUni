4c38b477cf416fe88baa7a6e4bfc2b87fefe1bc5
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-11-12 17:02:29 +0200

    Update 08_numbers_divisible.js

--- JS_Programming_Basics/04_For-Loop/lecture/08_numbers_divisible.js
+++ JS_Programming_Basics/04_For-Loop/lecture/08_numbers_divisible.js
@@ -1,3 +1,19 @@
 function divisible(input){
-    
+    let n1 = Number (input[0])
+    let n2 = Number (input[1])
+
+    let numbersDivisibleArray = []
+    let sum = 0
+    for (let i = n1; i <= n2; i++){
+        if (i % 9 == 0){
+            numbersDivisibleArray.push(i)
+            sum += i
+        }
+    }
+    console.log(`The sum: ${sum}`)
+    for (j = 0; j < numbersDivisibleArray.length; j++){
+        console.log(numbersDivisibleArray[j])
+    }
 }
+
+divisible(["100", "200"])
