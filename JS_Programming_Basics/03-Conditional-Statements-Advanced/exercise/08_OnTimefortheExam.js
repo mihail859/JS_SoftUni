82da6cea7f328c290477dd5a561c86865066155d
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-11-12 15:08:20 +0200

    Update 08_OnTimefortheExam.js

--- JS_Programming_Basics/03-Conditional-Statements-Advanced/exercise/08_OnTimefortheExam.js
+++ JS_Programming_Basics/03-Conditional-Statements-Advanced/exercise/08_OnTimefortheExam.js
@@ -9,19 +9,54 @@ function onTime(input){
     let timeInMinutesArrived = hourArrived * 60 + minuteArrived
 
     let message = ""
+    let info = ""
+    let difference = Math.abs(timeInMinutesArrived - timeInMinutesExam)
     if (timeInMinutesArrived > timeInMinutesExam){
         message += "Late"
+        if (difference < 60){
+            info += `${difference} minutes after the start`
+        }
+        else{
+            let hour = Math.floor(difference / 60)
+            let minutes = difference % 60
+            //hh:mm hours after the start
+            if (minutes < 10){
+                info += `${hour}:0${minutes} hours after the start`
+            }
+            else{
+                info += `${hour}:${minutes} hours after the start`
+            }
+        }
+
     }
     else{
-        let difference = Math.abs(timeInMinutesArrived - timeInMinutesExam)
+
         if (difference <= 30){
             message += "On time"
         }
         else{
             message += "Early"
         }
+        if (difference < 60){
+            info += `${difference} minutes before the start`
+        }
+        else{
+            let hour = Math.floor(difference / 60)
+            let minutes = difference % 60
+            //hh:mm hours after the start
+            if (minutes < 10){
+                info += `${hour}:0${minutes} hours before the start`
+            }
+            else{
+                info += `${hour}:${minutes} hours before the start`
+            }
+        }
     }
     console.log(message)
+    console.log(info)
+
+
+
 }
 onTime(["9",
 "30",
@@ -37,4 +72,10 @@ onTime(["16",
 "00",
 "15",
 "00"])
+
+onTime(["9",
+"00",
+"10",
+"30"])
+
 
