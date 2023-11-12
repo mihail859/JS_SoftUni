59de08ea20b4b1f66095bef91e03c1cc6cd8448d
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-11-12 15:43:13 +0200

    Create 03_numbers_with_Step3.js

---
+++ JS_Programming_Basics/04_For-Loop/lecture/03_numbers_with_Step3.js
@@ -0,0 +1,8 @@
+function printWithStep3(input){
+    let n = Number(input[0])
+    for (let index = 1; index <= n; index+=3){
+        console.log(index)
+    }
+}
+printWithStep3(["10"])
+printWithStep3(["15"])
