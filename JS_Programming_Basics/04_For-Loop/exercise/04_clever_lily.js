a906f5a5504daee08f786cc2dd7afccb8cfa5927
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-11-23 23:59:38 +0200

    Update 04_clever_lily.js

--- JS_Programming_Basics/04_For-Loop/exercise/04_clever_lily.js
+++ JS_Programming_Basics/04_For-Loop/exercise/04_clever_lily.js
@@ -3,12 +3,14 @@ function calculate(input){
     let washerPrice = Number (input[1])
     let toyPrice = Number (input[2])
 
+    let moneyReceived = 0
+    let toyReceived = 0
     for (let i = 1; i <= age; i++){
         if (i % 2 != 0){
 
         }
         else{
-            
+
         }
     }
 }
