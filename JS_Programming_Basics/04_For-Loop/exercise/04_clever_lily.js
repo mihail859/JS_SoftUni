6658a83699af8811e0ceceb1c025a13d28f7ab4f
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-11-24 00:08:27 +0200

    Update 04_clever_lily.js

--- JS_Programming_Basics/04_For-Loop/exercise/04_clever_lily.js
+++ JS_Programming_Basics/04_For-Loop/exercise/04_clever_lily.js
@@ -4,13 +4,26 @@ function calculate(input){
     let toyPrice = Number (input[2])
 
     let moneyReceived = 0
-    let toyReceived = 0
     for (let i = 1; i <= age; i++){
-        if (i % 2 != 0){
-
+        if (i % 2 == 0){
+            moneyReceived += i / 2 * 10
+            moneyReceived -= 1
         }
         else{
-
+            moneyReceived += toyPrice
         }
     }
+    let difference = Math.abs(moneyReceived - washerPrice)
+    if (washerPrice <= moneyReceived){
+        console.log(`Yes! ${difference.toFixed(2)}`)
+    }
+    else{
+        console.log(`No! ${difference.toFixed(2)}`)
+    }
 }
+
+
+calculate(["21",
+"1570.98",
+"3"])
+
