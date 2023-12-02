d434ae3ca36a50e256e5c9170aca008b1364d698
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-02 11:41:11 +0200

    Update 03_vacation.js

--- JS_Programming_Basics/05_While Loop/exercise/03_vacation.js
+++ JS_Programming_Basics/05_While Loop/exercise/03_vacation.js
@@ -1,3 +1,18 @@
 function moneyNeeded(input){
-    
+    let neededMoney = Number(input[0])
+    let availableMoney = Number(input[1])
+
+    let idx = 2
+    let daysInARowSpent = 0
+    while (true){
+        let action = input[idx]
+        let sum = Number(input[idx+1])
+
+        if (action === "spend"){
+
+        }
+        else{
+            
+        }
+    }
 }
