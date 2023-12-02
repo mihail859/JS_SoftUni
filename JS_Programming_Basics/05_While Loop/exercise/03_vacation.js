1363815c403e38cd10d5597a2946da6358220764
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-02 12:09:24 +0200

    Update 03_vacation.js

--- JS_Programming_Basics/05_While Loop/exercise/03_vacation.js
+++ JS_Programming_Basics/05_While Loop/exercise/03_vacation.js
@@ -4,15 +4,46 @@ function moneyNeeded(input){
 
     let idx = 2
     let daysInARowSpent = 0
+    let totalDays = 0
     while (true){
         let action = input[idx]
         let sum = Number(input[idx+1])
 
+        totalDays++;
+
         if (action === "spend"){
+            daysInARowSpent++;
+            if (sum > availableMoney){
+                availableMoney = 0
+            }
+            if (daysInARowSpent === 5){
+                console.log("You can't save the money.")
+                console.log(`${totalDays}`)
+                break
+            }
+            availableMoney -= sum
 
         }
         else{
-            
+            daysInARowSpent = 0
+            availableMoney += sum
+            if (availableMoney >= neededMoney){
+                console.log(`You saved the money for ${totalDays} days.`)
+                break
+            }
         }
+        idx+=2
     }
 }
+
+
+moneyNeeded(["250",
+"150",
+"spend",
+"50",
+"spend",
+"50",
+"save",
+"100",
+"save",
+"100"])
