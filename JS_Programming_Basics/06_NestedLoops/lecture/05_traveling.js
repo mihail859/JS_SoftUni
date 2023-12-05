09212fe78d5fbee01ae2a65fa0c6db52b7e2b41b
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-05 23:17:34 +0200

    Update 05_traveling.js

--- JS_Programming_Basics/06_NestedLoops/lecture/05_traveling.js
+++ JS_Programming_Basics/06_NestedLoops/lecture/05_traveling.js
@@ -1,3 +1,25 @@
 function trip(input){
-    
+    let idx = 0
+
+    let destination = input[0]
+    let minSum = Number (input[1])
+    while (true){
+        
+    }
 }
+
+trip(["Greece",
+"1000",
+"200",
+"200",
+"300",
+"100",
+"150",
+"240",
+"Spain",
+"1200",
+"300",
+"500",
+"193",
+"423",
+"End"])
