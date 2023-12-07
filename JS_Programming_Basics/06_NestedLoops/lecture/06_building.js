f05d3d7eaa9f33d419fb946255e6fcaec6e88976
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-07 22:12:04 +0200

    Update 06_building.js

--- JS_Programming_Basics/06_NestedLoops/lecture/06_building.js
+++ JS_Programming_Basics/06_NestedLoops/lecture/06_building.js
@@ -2,5 +2,8 @@ function seeBuilding(input){
     let floorsCount = Number (input[0])
     let roomsPerFloor = Number (input[1])
 
-    
+    for (let floor = floorsCount; floor >= 1; floor--){
+        console.log(floor)
+    }
+
 }
