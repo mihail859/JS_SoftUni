8a3730499adaa90d8084ca25dd316d13c2ff2cb0
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-08 23:48:10 +0200

    Update 01_number_pyramid.js

--- JS_Programming_Basics/06_NestedLoops/exercises/01_number_pyramid.js
+++ JS_Programming_Basics/06_NestedLoops/exercises/01_number_pyramid.js
@@ -1,10 +1,13 @@
 function drawPyramid(input){
     let n = Number (input[0])
-
+    let number = 0
     let printLine = ""
+
+    
     for (row = 1; row <= n; row++){
         for (let col = 1; col <= row; col++){
-            printLine += " " + col
+            number++;
+            printLine += " " + number
         }
         console.log(printLine)
         printLine = ""
