41db9e2697ebd5d76c00030e73ec2637cbc28ce9
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-08 23:50:34 +0200

    Update 01_number_pyramid.js

--- JS_Programming_Basics/06_NestedLoops/exercises/01_number_pyramid.js
+++ JS_Programming_Basics/06_NestedLoops/exercises/01_number_pyramid.js
@@ -3,17 +3,26 @@ function drawPyramid(input){
     let number = 0
     let printLine = ""
 
-    
+    let isBigger = false
     for (row = 1; row <= n; row++){
         for (let col = 1; col <= row; col++){
             number++;
+            if (number > n){
+                isBigger = true
+                break
+            }
             printLine += " " + number
         }
         console.log(printLine)
         printLine = ""
+        if (isBigger){
+            break
+        }
     }
 }
 
 drawPyramid(["7"])
-/*drawPyramid(["12"])
-drawPyramid(["15"])*/
+console.log()
+drawPyramid(["12"])
+console.log()
+drawPyramid(["15"])
