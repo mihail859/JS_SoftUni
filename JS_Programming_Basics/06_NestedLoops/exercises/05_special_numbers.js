97e8e03964da267d1ca709505739ae0f9e3db6dd
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-09 21:46:39 +0200

    Update 05_special_numbers.js

--- JS_Programming_Basics/06_NestedLoops/exercises/05_special_numbers.js
+++ JS_Programming_Basics/06_NestedLoops/exercises/05_special_numbers.js
@@ -1,3 +1,27 @@
 function printSpecialNumbers(input){
-    
+    let n = Number (input[0])
+
+    let specialNumbersArray = []
+    let isSpecial = true
+
+    for (let i = 1111; i <= 9999; i++){
+        let currentNumber  = i.toString()
+        for (let j = 0; j<4; j++){
+            let currentDigit = Number (currentNumber[j])
+            if (n % currentDigit != 0){
+                isSpecial = false
+                break
+            }
+        }
+        if (isSpecial){
+            specialNumbersArray.push(currentNumber)
+        }
+        else{
+            isSpecial = true
+        }
+    }
+    console.log(specialNumbersArray)
+
 }
+
+printSpecialNumbers(["3"])
