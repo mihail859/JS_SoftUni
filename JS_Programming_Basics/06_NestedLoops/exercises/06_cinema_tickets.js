c17dc21d7727d562bcd55b08a794a49d2be774fa
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-09 22:07:41 +0200

    Update 06_cinema_tickets.js

--- JS_Programming_Basics/06_NestedLoops/exercises/06_cinema_tickets.js
+++ JS_Programming_Basics/06_NestedLoops/exercises/06_cinema_tickets.js
@@ -36,6 +36,17 @@ function ticketsStatistics(input){
         console.log(`${moveName} - ${percentageFull.toFixed(2)}% full.`)
         idx++
     }
+
+    let totalTicketsSold = studentTicketsSold + standardTicketsSold + kidTicketsSold
+    let percentStudentTickets = studentTicketsSold / totalTicketsSold * 100
+    let percentStandardTickets = standardTicketsSold / totalTicketsSold * 100
+    let percentKidTickets = kidTicketsSold / totalTicketsSold * 100
+
+    console.log(`Total tickets: ${totalTicketsSold}`)
+    console.log(`${percentStudentTickets.toFixed(2)}% student tickets.`)
+    console.log(`${percentStandardTickets.toFixed(2)}% standard tickets.`)
+    console.log(`${percentKidTickets.toFixed(2)}% kids tickets.`)
+    
 }
 
 ticketsStatistics(["Taxi",
@@ -56,4 +67,34 @@ ticketsStatistics(["Taxi",
 "student",
 "student",
 "Finish"])
+
+console.log("---------------------------------------")
+
+ticketsStatistics(["The Matrix",
+"20",
+"student",
+"standard",
+"kid",
+"kid",
+"student",
+"student",
+"standard",
+"student",
+"End",
+"The Green Mile",
+"17",
+"student",
+"standard",
+"standard",
+"student",
+"standard",
+"student",
+"End",
+"Amadeus",
+"3",
+"standard",
+"standard",
+"standard",
+"Finish"])
+
 
