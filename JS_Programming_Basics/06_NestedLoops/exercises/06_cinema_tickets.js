503f84a3e18a6c522e393fbf7892e713c3904135
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-09 21:53:22 +0200

    Update 06_cinema_tickets.js

---
+++ JS_Programming_Basics/06_NestedLoops/exercises/06_cinema_tickets.js
@@ -0,0 +1,34 @@
+function ticketsStatistics(input){
+
+    let idx = 0
+    while (true){
+        let moveName = input[idx]
+        let seatsForMove = Number(input[idx+1])
+
+        for (let s = 0; s < seatsForMove; s++){
+            
+        }
+
+
+    }
+}
+
+ticketsStatistics(["Taxi",
+"10",
+"standard",
+"kid",
+"student",
+"student",
+"standard",
+"standard",
+"End",
+"Scary Movie",
+"6",
+"student",
+"student",
+"student",
+"student",
+"student",
+"student",
+"Finish"])
+
