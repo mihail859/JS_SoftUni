852a6d5972e580b4149d02ca993b1cb1896e977a
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-09 22:01:29 +0200

    Update 06_cinema_tickets.js

--- JS_Programming_Basics/06_NestedLoops/exercises/06_cinema_tickets.js
+++ JS_Programming_Basics/06_NestedLoops/exercises/06_cinema_tickets.js
@@ -1,15 +1,40 @@
 function ticketsStatistics(input){
 
+    let studentTicketsSold = 0
+    let standardTicketsSold = 0
+    let kidTicketsSold = 0
+
     let idx = 0
     while (true){
         let moveName = input[idx]
-        let seatsForMove = Number(input[idx+1])
+        if (moveName === "Finish"){
+            break
+        }
+        idx++
+        let seatsForMove = Number(input[idx])
+        let seatsSold = 0
 
         for (let s = 0; s < seatsForMove; s++){
-            
+            idx++
+            let typeTicket = input[idx]
+            if (typeTicket === "End"){
+                break
+            }
+            else if (typeTicket === "student"){
+                studentTicketsSold++;
+            }
+            else if (typeTicket === "standard"){
+                standardTicketsSold++;
+            }
+            else{
+                kidTicketsSold++;
+            }
+            seatsSold++;
+
         }
-
-
+        let percentageFull = seatsSold / seatsForMove * 100
+        console.log(`${moveName} - ${percentageFull.toFixed(2)}% full.`)
+        idx++
     }
 }
 
