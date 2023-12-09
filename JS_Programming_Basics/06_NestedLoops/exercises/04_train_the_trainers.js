d69d7c991314d49d60ade4272450159da46c4d28
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-09 14:13:08 +0200

    Update 04_train_the_trainers.js

---
+++ JS_Programming_Basics/06_NestedLoops/exercises/04_train_the_trainers.js
@@ -0,0 +1,15 @@
+function printResult(input){
+    let numberOfJudges = Number (input[0])
+
+    
+    
+}
+
+printResult(["2",
+"While-Loop",
+"6.00",
+"5.50",
+"For-Loop",
+"5.84",
+"5.66",
+"Finish"])
