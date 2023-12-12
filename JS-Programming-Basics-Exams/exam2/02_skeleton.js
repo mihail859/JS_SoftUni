1570e096e82a66ad110c4665e3f40246399af577
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-12 23:06:07 +0200

    Update 02_skeleton.js

--- JS-Programming-Basics-Exams/exam2/02_skeleton.js
+++ JS-Programming-Basics-Exams/exam2/02_skeleton.js
@@ -4,5 +4,18 @@ function printStatement(input){
     let lengthTrace = Number (input[2])
     let secondsPer100Meters = Number (input[3])
 
-    
+    let controlTimeInSeconds = 60*minuteControl + secondsControl
+    let timesTimeDecrease = Math.floor(lengthTrace / 120)
+    let totalDecreased = timesTimeDecrease * 2.5
+    let timeAchieved = (lengthTrace / 100) * secondsPer100Meters - totalDecreased
+
+    if (timeAchieved <= controlTimeInSeconds){
+        console.log("Marin Bangiev won an Olympic quota!")
+        console.log(`His time is ${timeAchieved.toFixed(3)}.`)
+    }
+    else{
+        let neededSeconds = timeAchieved - controlTimeInSeconds
+        console.log(`No, Marin failed! He was ${neededSeconds.toFixed(3)} second slower`)
+    }
+
 }
