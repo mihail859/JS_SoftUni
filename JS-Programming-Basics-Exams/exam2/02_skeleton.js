ab99f9ca172f52ef1cab9f0d51cc05118c141df1
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-12 22:57:57 +0200

    Update 02_skeleton.js

--- JS-Programming-Basics-Exams/exam2/02_skeleton.js
+++ JS-Programming-Basics-Exams/exam2/02_skeleton.js
@@ -1,3 +1,8 @@
 function printStatement(input){
+    let minuteControl = Number (input[0])
+    let secondsControl = Number (input[1])
+    let lengthTrace = Number (input[2])
+    let secondsPer100Meters = Number (input[3])
+
     
 }
