3c044668037eba5d3b25408dc6a375abefe0f698
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-12 20:35:01 +0200

    Update 01_tennis_equipment.js

--- JS-Programming-Basics-Exams/exam1/01_tennis_equipment.js
+++ JS-Programming-Basics-Exams/exam1/01_tennis_equipment.js
@@ -1,3 +1,29 @@
 function calculate(input){
-    
+    let tennisRacketPrice = Number (input[0])
+    let numberOfTennisRacket = Number (input[1])
+    let numberOfShoesPairs = Number (input[2])
+
+    let pairShoesPrice = 1 / 6 * tennisRacketPrice
+
+    let totalTennisRacketSum = tennisRacketPrice * numberOfTennisRacket
+    let totalShoesSum = numberOfShoesPairs * pairShoesPrice
+
+
+    let totalPrice = totalShoesSum + totalTennisRacketSum
+    let equipmentPrice = totalPrice * 0.20
+    totalPrice += equipmentPrice
+
+    let pricePaidByHim = totalPrice * 1/8
+    let leftPrice = totalPrice * 7/8
+
+    console.log(`Price to be paid by Djokovic ${Math.floor(pricePaidByHim)}`)
+    console.log(`Price to be paid by sponsors ${Math.ceil(leftPrice)}`)
+
 }
+
+
+calculate(
+    ["850",
+    "4",
+    "2"]
+)
