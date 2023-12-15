2f9f621073249a1a4722f41b7797403eb8f6e664
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-15 21:35:13 +0200

    Update 04_darts.js

--- JS-Programming-Basics-Exams/exam2/04_darts.js
+++ JS-Programming-Basics-Exams/exam2/04_darts.js
@@ -1,11 +1,42 @@
 function dartsGame(input){
     let playerName = input[0]
 
+    let playerPoints = 301
+    let trows = 0
+    let unsuccessfulTrows = 0
+
     let idx = 1
     while (true){
         if (input[idx] === "Retire"){
-            
+            break
         }
+        let sector = input[idx]
+        let points = Number (input[idx + 1])
+
+        let currentPoints = 0
+
+        switch(sector){
+            case "Single":
+                currentPoints += points
+                break
+            case "Double":
+                currentPoints += 2*points
+                break
+            case "Triple":
+                currentPoints += 3*points
+                break               
+        }
+
+        if (currentPoints <= playerPoints){
+            trows++;
+            playerPoints -= currentPoints
+            if (playerPoints === 0){
+
+            }
+        }
+
+
+        idx+=2
     }
 
 }
