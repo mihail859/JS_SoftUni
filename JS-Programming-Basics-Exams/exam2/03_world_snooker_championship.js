9f4a2be899aad6bfbbb80a1e33edcd3a8bff04bc
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-15 14:14:04 +0200

    update

---
+++ JS-Programming-Basics-Exams/exam2/03_world_snooker_championship.js
@@ -0,0 +1,21 @@
+function solve(input){
+    let stage = input[0]
+    let ticketType = input[1]
+    let countTickets = Number (input[2])
+    let pictureWithTrophy = input[3]
+}
+solve(["Final",
+"Premium",
+"25",
+"Y"])
+
+solve(["Semi final",
+"VIP",
+"9",
+"Y"])
+
+solve(["Quarter final",
+"Standard",
+"11",
+"N"])
+
