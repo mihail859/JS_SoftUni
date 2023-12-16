ae5c3bed46bed3159b5b24a4a8588bf64fcf7499
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-16 23:16:33 +0200

    Create 04_foreign_languages.js

---
+++ JS_Programming_Fundamentals/Lab_JS_Introduction/04_foreign_languages.js
@@ -0,0 +1,14 @@
+function speak(country){
+    if (country === "England" | country === "USA"){
+        console.log('English')
+    }
+    else if (country === "Spain" | country === "Argentina" | country === "Mexico"){
+        console.log("Spanish")
+    }
+    else{
+        console.log('unknown')
+    }
+}
+
+speak('USA')
+speak('Germany')
