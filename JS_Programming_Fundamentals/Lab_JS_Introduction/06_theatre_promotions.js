112b6db4192232de563810a7bbf2379eb48a19a9
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-17 10:20:35 +0200

    Update 06_theatre_promotions.js

--- JS_Programming_Fundamentals/Lab_JS_Introduction/06_theatre_promotions.js
+++ JS_Programming_Fundamentals/Lab_JS_Introduction/06_theatre_promotions.js
@@ -1,1 +1,52 @@
-s
+function ticketsPromotion(dayType, age){
+    let ticketPrice = ""
+    switch(dayType){
+        case "Weekday":
+            if (age >=0 & age <= 18){
+                ticketPrice = "12$"
+            }
+            else if (age <= 64 & age > 18){
+                ticketPrice = "18$"
+            }
+            else if (age > 64 & age <= 122){
+                ticketPrice = "12$"
+            }
+            else{
+                ticketPrice = "Error!"
+            }
+            break
+        case "Weekend":
+            if (age >=0 & age <= 18){
+                ticketPrice = "15$"
+            }
+            else if (age <= 64 & age > 18){
+                ticketPrice = "20$"
+            }
+            else if (age > 64 & age <= 122){
+                ticketPrice = "15$"
+            }
+            else{
+                ticketPrice = "Error!"
+            }
+            break
+        case "Holiday":
+            if (age >=0 & age <= 18){
+                ticketPrice = "5$"
+            }
+            else if (age <= 64 & age > 18){
+                ticketPrice = "12$"
+            }
+            else if (age > 64 & age <= 122){
+                ticketPrice = "10$"
+            }
+            else{
+                ticketPrice = "Error!"
+            }
+            break 
+    }
+    console.log(ticketPrice)
+}
+
+ticketsPromotion('Weekday', 42)
+ticketsPromotion('Holiday', -12)
+ticketsPromotion('Holiday', 15)
