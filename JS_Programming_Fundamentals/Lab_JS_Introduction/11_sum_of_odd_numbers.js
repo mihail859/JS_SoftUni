db9e29cdceaab545816d1944019b7754f2b047dc
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-17 22:06:40 +0200

    Update 11_sum_of_odd_numbers.js

--- JS_Programming_Fundamentals/Lab_JS_Introduction/11_sum_of_odd_numbers.js
+++ JS_Programming_Fundamentals/Lab_JS_Introduction/11_sum_of_odd_numbers.js
@@ -1,5 +1,14 @@
-function sumOfOdd(number){
-
+function sumOfOdd(count){
+    let totalSum = 0
+    let currentNumber = 1
+    while (count >0){
+        totalSum += currentNumber
+        totalSum += currentNumber
+        console.log(currentNumber)
+        currentNumber+=2
+        count--;
+    }
+    console.log(: `Sum: {total sum}`)
 }
 sumOfOdd(5)
 sumOfOdd(3)
