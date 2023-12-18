420d39ce92e25da6b8f4a0f652434e1a2ce65d2a
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-18 16:35:38 +0200

    Update 05_leap_year.js

--- JS_Programming_Fundamentals/Exercise_JS_introduction/05_leap_year.js
+++ JS_Programming_Fundamentals/Exercise_JS_introduction/05_leap_year.js
@@ -1,3 +1,16 @@
 function isYearLeap(year){
-    
+    let isDivisible = false
+    if ((year % 4 == 0 & year % 100 != 0) | year % 400 === 0){
+        isDivisible = true
+    }
+    if (isDivisible){
+        console.log('yes')
+    }
+    else{
+        console.log('no')
+    }
 }
+
+isYearLeap(1984)
+isYearLeap(2003)
+isYearLeap(4)
