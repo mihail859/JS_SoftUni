ed8cade7475c1ffee12c82a66f6370dca7d5f195
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-18 16:40:52 +0200

    Update 06_print_and_sum.js

--- JS_Programming_Fundamentals/Exercise_JS_introduction/06_print_and_sum.js
+++ JS_Programming_Fundamentals/Exercise_JS_introduction/06_print_and_sum.js
@@ -1,3 +1,13 @@
 function displayNumbersInRange(start, end){
+    let sumNumbers = 0
+    let numbersRow = ""
+
+    for (let i = start; i <= end; i++){
+        numbersRow += i + " "     
+        sumNumbers += i
+    }
+    console.log(numbersRow)
+    console.log(`Sum: ${sumNumbers}`)
 
 }
+displayNumbersInRange(5, 10)
