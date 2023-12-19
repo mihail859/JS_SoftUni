4f53b7b34e7a67fad09eab4d088aba50a24e46ad
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-19 09:37:51 +0200

    Update 09_login.js

--- JS_Programming_Fundamentals/Exercise_JS_introduction/09_login.js
+++ JS_Programming_Fundamentals/Exercise_JS_introduction/09_login.js
@@ -1,3 +1,26 @@
-function logIN(){
-    
+function logIN(input){
+    let password = input[0]
+    let tries = 0
+
+    let idx = 1
+    for (let i = idx ; i <= input.length; i++){
+        tries++;
+
+        let tryPassword = input[idx]
+        let reversedPasswordTry = Array.from(tryPassword).reverse().join('');
+        if (reversedPasswordTry === password){
+            console.log(`User ${password} logged in.`)
+            break
+        }
+        else if(tries === 4){
+            console.log(`User ${password} blocked!`)
+            break
+        }
+        else{
+            console.log('Incorrect password. Try again.')
+        }
+        idx++;
+    }
 }
+
+logIN(['Acer','login','go','let me in','recA'])
