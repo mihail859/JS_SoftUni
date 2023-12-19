884dfb1b4b1e4a07b2733a398cffb88f0a86702a
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-19 09:09:49 +0200

    Update 07_triangle_of_numbers.js

--- JS_Programming_Fundamentals/Exercise_JS_introduction/07_triangle_of_numbers.js
+++ JS_Programming_Fundamentals/Exercise_JS_introduction/07_triangle_of_numbers.js
@@ -1,4 +1,15 @@
 function pyramidOfNumbers(n){
+    let number = 1
+    let printNumbers = ""
+    
+    for (let i = 1; i <= n; i++){
+        for (j = 1; j <= i; j++){
+            printNumbers += number + " "
+        }
 
+        console.log(printNumbers)
+        printNumbers = ""
+        number++;
+    }   
 }
 pyramidOfNumbers(7)
