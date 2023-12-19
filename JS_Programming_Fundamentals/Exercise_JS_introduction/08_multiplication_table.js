ebf89b466bd475316cbf1a098a5fefa3388952ea
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-19 09:15:50 +0200

    Update 08_multiplication_table.js

--- JS_Programming_Fundamentals/Exercise_JS_introduction/08_multiplication_table.js
+++ JS_Programming_Fundamentals/Exercise_JS_introduction/08_multiplication_table.js
@@ -1,3 +1,7 @@
 function multiplication(number){
-    
+    for (let i = 1; i <= 10; i++){
+        let result = number * i
+        console.log(`${number} X ${i} = ${result}`)
+    }
 }
+multiplication(5)
