b000cbdd3c121135786e4c8839f691ac66ddcb34
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-20 17:50:06 +0200

    Update 03_right_place.js

--- JS_Programming_Fundamentals/02_Lab_Data Types and Variables/03_right_place.js
+++ JS_Programming_Fundamentals/02_Lab_Data Types and Variables/03_right_place.js
@@ -1,3 +1,9 @@
-function solve(string, char, string){
-    
+function solve(string_old, char, string){
+    let result = string_old.replace('_', char)
+    let output =  result === string ? "Matched" : "Not matched"
+    console.log(output)
 }
+
+solve('Str_ng', 'I', 'Strong')
+console.log("--------------------")
+solve('Str_ng', 'i', 'String')
