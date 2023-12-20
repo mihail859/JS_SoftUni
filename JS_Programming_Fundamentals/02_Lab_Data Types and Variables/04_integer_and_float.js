8646638c8aacade4e2aade39614cd318c3a67c01
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-20 17:56:38 +0200

    Update 04_integer_and_float.js

--- JS_Programming_Fundamentals/02_Lab_Data Types and Variables/04_integer_and_float.js
+++ JS_Programming_Fundamentals/02_Lab_Data Types and Variables/04_integer_and_float.js
@@ -1,3 +1,9 @@
-function solve(){
-    
+function solve(number1, number2, number3){
+    let sum = number1 + number2 + number3
+
+    sum % 1 === 0 ? sum += ' - Integer' : sum += ' - Float'
+    console.log(sum)
 }
+
+solve(9, 100, 1.1)
+solve(100, 200, 303)
