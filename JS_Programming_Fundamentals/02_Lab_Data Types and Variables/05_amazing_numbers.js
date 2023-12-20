99acb986f9634f3be881103b764000c13501482e
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-20 18:07:19 +0200

    Update 05_amazing_numbers.js

--- JS_Programming_Fundamentals/02_Lab_Data Types and Variables/05_amazing_numbers.js
+++ JS_Programming_Fundamentals/02_Lab_Data Types and Variables/05_amazing_numbers.js
@@ -1,3 +1,12 @@
 function amazingNUmbersCheck(number){
-    
+    number = number.toString()
+    let sum = 0
+    for (let i = 0; i < number.length; i++){
+        sum += Number (number[i])
+    }
+    let result = sum.toString().includes('9')
+    console.log(result ? `${number} Amazing? True`: `${number} Amazing? False`)
 }
+
+amazingNUmbersCheck(1233)
+amazingNUmbersCheck(999)
