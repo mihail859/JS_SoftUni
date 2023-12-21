f85ecd5448a16610cf3c200b08d5f30f4a4ff92a
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-21 21:33:28 +0200

    Update 07_required_reading.js

--- JS_Programming_Fundamentals/02_Lab_Data Types and Variables/07_required_reading.js
+++ JS_Programming_Fundamentals/02_Lab_Data Types and Variables/07_required_reading.js
@@ -1,3 +1,9 @@
 function timeNeeded(pages, pagesPerHour, daysLimitToRead){
-     
+    let neededHoursToReadTheBook = pages / pagesPerHour
+    let requiredHours = neededHoursToReadTheBook / daysLimitToRead
+    console.log(`${requiredHours.toFixed(1)}`)
 }
+timeNeeded(212,
+    20 ,
+    2 
+    )
