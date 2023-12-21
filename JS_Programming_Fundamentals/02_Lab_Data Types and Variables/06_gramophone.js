b63ba7c895af34d0cd7a5f688c34b179cd96b8bd
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-21 18:21:35 +0200

    Update 06_gramophone.js

--- JS_Programming_Fundamentals/02_Lab_Data Types and Variables/06_gramophone.js
+++ JS_Programming_Fundamentals/02_Lab_Data Types and Variables/06_gramophone.js
@@ -1,3 +1,11 @@
-function gramophoneFunction(){
-    
+function gramophoneFunction(band, album, song){
+    /**
+     * (albumName.length * bandName.length) * song-name.length / 2
+     */
+    let songDuration = ((album.length * band.length) * song.length / 2) / 2.5
+    let output = `The plate was rotated ${Math.ceil(songDuration)} times.`
+    console.log(output)
 }
+
+gramophoneFunction('Black Sabbath', 'Paranoid', 'War Pigs')
+gramophoneFunction('Rammstein', 'Sehnsucht', 'Engel')
