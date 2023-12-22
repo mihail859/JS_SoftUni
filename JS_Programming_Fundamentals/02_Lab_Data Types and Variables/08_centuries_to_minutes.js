a96b19ea4b29a857408ba65cdd097d0bc01e8b73
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-22 11:12:38 +0200

    Update 08_centuries_to_minutes.js

--- JS_Programming_Fundamentals/02_Lab_Data Types and Variables/08_centuries_to_minutes.js
+++ JS_Programming_Fundamentals/02_Lab_Data Types and Variables/08_centuries_to_minutes.js
@@ -1,3 +1,11 @@
 function convert(centuries){
-    
+    let years = centuries * 100
+    let days = years * 365.24220
+    let hours = days * 24
+    let minutes = hours * 60
+
+    console.log(`${centuries} centuries = ${years} years = ${days} days = ${hours} hours = ${minutes} minutes`)
 }
+
+convert(1)
+convert(5)
