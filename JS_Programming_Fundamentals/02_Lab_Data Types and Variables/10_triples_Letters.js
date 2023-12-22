b8b3f614d01a41ea8a2ffb4e2bac367f49358174
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-22 21:23:39 +0200

    Update 10_triples_Letters.js

--- JS_Programming_Fundamentals/02_Lab_Data Types and Variables/10_triples_Letters.js
+++ JS_Programming_Fundamentals/02_Lab_Data Types and Variables/10_triples_Letters.js
@@ -1,9 +1,11 @@
 function printTriple(n){
-    n+=97
+    n = Number (n)
+    n+= 97
     for(let i = 97; i < n; i++){
         for(let j= 97; j <n; j++){
             for(let k= 97; k < n; k++){
-                console.log(String.fromCharCode(i,j,k))
+                let output = String.fromCharCode(i,j,k)
+                console.log(output)
             }
         }
     }
