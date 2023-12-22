65b79cf7820d2576e3d166b3956df19d5f2131c5
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-22 11:43:43 +0200

    Update 09_special_numbers.js

--- JS_Programming_Fundamentals/02_Lab_Data Types and Variables/09_special_numbers.js
+++ JS_Programming_Fundamentals/02_Lab_Data Types and Variables/09_special_numbers.js
@@ -1,3 +1,19 @@
-function special(){
-    
+function special(number){
+    let sumOfDigits = 0
+    for (let i = 1; i <= number; i++){
+        let numberAsString = i.toString()
+        for (let j = 0; j < numberAsString.length; j++){
+            let currentDigit = Number (numberAsString[j])
+            sumOfDigits += currentDigit
+        }
+        if (sumOfDigits === 5 | sumOfDigits === 7 | sumOfDigits === 11){
+            console.log(`${i} -> True`)
+        }
+        else{
+            console.log(`${i} -> False`)
+        }
+        sumOfDigits = 0
+    }
 }
+
+special(15)
