f153e3d9f839554574e896140f8cce5e6c9b159a
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-23 19:23:35 +0200

    Update 01_sum_digits.js

--- JS_Programming_Fundamentals/02_Data Types and Variables - Exercise/01_sum_digits.js
+++ JS_Programming_Fundamentals/02_Data Types and Variables - Exercise/01_sum_digits.js
@@ -1,3 +1,11 @@
 function sumDigitsOfNumber(number){
-    
+    let stringNumber = number.toString()
+    let sumDigits = 0
+    for (let i = 0; i < stringNumber.length; i++){
+        let currentDigit = Number (stringNumber[i])
+        sumDigits += currentDigit
+    }
+    console.log(sumDigits)
 }
+
+sumDigitsOfNumber(245678)
