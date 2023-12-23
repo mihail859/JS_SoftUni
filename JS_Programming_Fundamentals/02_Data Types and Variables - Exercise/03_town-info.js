ac29a414582db4717226d183207aea59a84b0e8d
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-23 19:30:40 +0200

    Update 03_town-info.js

---
+++ JS_Programming_Fundamentals/02_Data Types and Variables - Exercise/03_town-info.js
@@ -0,0 +1,12 @@
+function printInformation(town, population, area){
+    console.log(`Town ${town} has population of ${population} and area ${area} square km.`)
+}
+
+printInformation('Sofia',
+1286383,
+492
+)
+
+printInformation('Plovdiv',
+1481353,
+512)
