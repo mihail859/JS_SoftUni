996d17c75c35d5f1d8b02129d08f788eb6a0ca8f
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-24 08:35:42 +0200

    Update 07_lower_or_upper.js

---
+++ JS_Programming_Fundamentals/02_Data Types and Variables - Exercise/07_lower_or_upper.js
@@ -0,0 +1,10 @@
+function lowerOrUpper(character){
+    if (character === character.toUpperCase()){
+        console.log('upper-case')
+    }
+    else if (character === character.toLowerCase()){
+        console.log('lower-case')
+    }
+}
+
+lowerOrUpper('L')
