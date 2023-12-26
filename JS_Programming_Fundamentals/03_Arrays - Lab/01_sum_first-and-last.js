f147ba06bc76770402edd35f76e24dbf285d1bf6
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-26 11:07:32 +0200

    Update 01_sum_first-and-last.js

---
+++ JS_Programming_Fundamentals/03_Arrays - Lab/01_sum_first-and-last.js
@@ -0,0 +1,5 @@
+function sumFirstAndLastElement(arrayNumbers){
+    let sumNumbers = arrayNumbers[0] + arrayNumbers[arrayNumbers.length - 1]
+    console.log(sumNumbers)
+}
+sumFirstAndLastElement([20, 30, 40])
