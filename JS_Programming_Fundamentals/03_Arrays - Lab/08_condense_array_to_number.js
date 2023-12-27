85ccd8b417c1249d2e6e44ef8b33ac77adb2b1ad
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-27 13:28:21 +0200

    Update 08_condense_array_to_number.js

--- JS_Programming_Fundamentals/03_Arrays - Lab/08_condense_array_to_number.js
+++ JS_Programming_Fundamentals/03_Arrays - Lab/08_condense_array_to_number.js
@@ -1,5 +1,15 @@
 function solve(arrayNumbers){
     let condensed = []
 
+    while (arrayNumbers.length > 1){
+        for (let i = 0; i < arrayNumbers.length - 1; i++){
+            condensed[i] = arrayNumbers[i] + arrayNumbers[i+1]
+        }   
+        arrayNumbers = condensed
+        condensed = []
+    }
+    console.log(arrayNumbers.toString())
 
 }
+
+solve([2, 10, 3])
