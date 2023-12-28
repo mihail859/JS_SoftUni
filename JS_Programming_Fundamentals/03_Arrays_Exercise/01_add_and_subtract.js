3f3b974e890bf3b3a17c34979772e57668bbf285
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-28 21:05:08 +0200

    Update 01_add_and_subtract.js

--- JS_Programming_Fundamentals/03_Arrays_Exercise/01_add_and_subtract.js
+++ JS_Programming_Fundamentals/03_Arrays_Exercise/01_add_and_subtract.js
@@ -2,7 +2,14 @@ function solve(arrayNumbers){
     let sumBefore = 0
     let sumAfter = 0
 
-    for (let number of arrayNumbers){
-
+    for (let i = 0; i < arrayNumbers.length; i++){
+        let currentElement = arrayNumbers[i]
+        sumBefore += currentElement
+        if (currentElement % 2 === 0){
+            currentElement += i
+        }
+        else{
+            currentElement -= i
+        }
     }
 }
