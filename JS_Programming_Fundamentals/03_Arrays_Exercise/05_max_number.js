3bfff7e85f142d50cc24b20da7ba1a93b5fea991
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-29 11:33:06 +0200

    Update 05_max_number.js

--- JS_Programming_Fundamentals/03_Arrays_Exercise/05_max_number.js
+++ JS_Programming_Fundamentals/03_Arrays_Exercise/05_max_number.js
@@ -1,3 +1,24 @@
 function maxNumber(arrayNumbers){
-    
+    let topIntegers = []
+    let isTopInteger = true
+    for (let i = 0; i < arrayNumbers.length; i++){
+        let currentNumber = arrayNumbers[i]
+
+        for (let j = i+1; j < arrayNumbers.length; j++){
+            let compareNumber = arrayNumbers[j]
+            if (currentNumber <= compareNumber){
+                isTopInteger = false
+                break
+            }
+        }
+        if (isTopInteger){
+            topIntegers.push(currentNumber)
+        }
+        else{
+            isTopInteger = true
+        }
+    }
+    console.log(topIntegers.join(" "))
 }
+
+maxNumber([1, 4, 3, 2])
