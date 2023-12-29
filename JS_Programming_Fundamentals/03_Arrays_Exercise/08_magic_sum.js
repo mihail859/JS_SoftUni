2b49ab8a2daefdd755a12ff36276ff077f95511c
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-29 16:39:15 +0200

    Update 08_magic_sum.js

--- JS_Programming_Fundamentals/03_Arrays_Exercise/08_magic_sum.js
+++ JS_Programming_Fundamentals/03_Arrays_Exercise/08_magic_sum.js
@@ -1,3 +1,19 @@
-function magicSum(arrayNumbers, n){
-    
+function magicSum(arrayNumbers, n) {
+    for (let i = 0; i < arrayNumbers.length; i++) {
+        let currentNumber = arrayNumbers[i]
+        for (let j = i+1; j < arrayNumbers.length; j++){
+            let nextNumber = arrayNumbers[j]
+            if (currentNumber + nextNumber === n){
+                console.log(`${currentNumber} ${nextNumber}`)
+            }
+        }
+    }
 }
+
+magicSum([1, 7, 6, 2, 19, 23],
+    8
+)
+console.log("------***-----")
+magicSum([14, 20, 60, 13, 7, 19, 8],
+    27
+    )
