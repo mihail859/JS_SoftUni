118727f0b6744610201edf8c325d291b01f9da3f
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-29 16:26:51 +0200

    Update 07_max_sequence_of_equal_elements.js

--- JS_Programming_Fundamentals/03_Arrays_Exercise/07_max_sequence_of_equal_elements.js
+++ JS_Programming_Fundamentals/03_Arrays_Exercise/07_max_sequence_of_equal_elements.js
@@ -1,5 +1,23 @@
-function maxSequence(arraySequence){
+function findLongestSequence(arr) {
+    let currentSequence = [arr[0]];
+    let longestSequence = [arr[0]];
 
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] === arr[i - 1]) {
+            currentSequence.push(arr[i]);
+        } else {
+            currentSequence = [arr[i]];
+        }
+
+        if (currentSequence.length > longestSequence.length) {
+            longestSequence = [...currentSequence];
+        }
+    }
+
+    console.log(longestSequence.join(" "));
 }
 
-arraySequence([2, 1, 1, 2, 3, 3, 2, 2, 2, 1])
+
+
+findLongestSequence([2, 1, 1, 2, 3, 3, 2, 2, 2, 1]);
+
