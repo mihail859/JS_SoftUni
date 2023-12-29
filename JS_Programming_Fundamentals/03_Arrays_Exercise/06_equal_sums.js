561ce32b35d27d9ceb95452113207b0805898009
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-29 12:22:36 +0200

    Update 06_equal_sums.js

--- JS_Programming_Fundamentals/03_Arrays_Exercise/06_equal_sums.js
+++ JS_Programming_Fundamentals/03_Arrays_Exercise/06_equal_sums.js
@@ -1,4 +1,4 @@
-function solve(arr){
+/*function solve(arr){
     let position = 'no'
     arr.forEach((e, i, arrs) => {
       let newArr = arrs.slice(0)
@@ -12,4 +12,43 @@ function solve(arr){
     function sumArrays(a){
         return a.reduce((acc, e) => acc + e, 0)
     }
-  }
+  }*/
+
+function arraysDemo(arr) {
+
+    let leftSum = 0;
+    let rightSum = 0;
+    let isEqual = false;
+    let arrayLength = arr.length;
+
+    for (let currentIndex = 0; currentIndex < arrayLength; currentIndex++) {
+        for (let i = 0; i < arrayLength; i++) {
+
+            if (i !== currentIndex) {
+                let num = Number(arr[i]);
+                if (i < currentIndex) {
+                    leftSum += num;
+                } else if (i > currentIndex) {
+                    rightSum += num;
+                }
+            }
+            if (currentIndex === 0) {
+                leftSum = 0;
+            } else if (currentIndex === arrayLength - 1) {
+                rightSum = 0;
+            }
+        }
+
+        if (leftSum === rightSum) {
+            console.log(currentIndex);
+            isEqual = true;
+        } else {
+            leftSum = 0;
+            rightSum = 0;
+        }
+    }
+    if (!isEqual) {
+        console.log('no');
+    }
+}
+arraysDemo([1, 2, 3, 3]);
