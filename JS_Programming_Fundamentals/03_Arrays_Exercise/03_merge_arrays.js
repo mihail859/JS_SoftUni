7a67dff68d3b400116896ad381200338b087ef18
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-29 10:51:36 +0200

    Update 03_merge_arrays.js

--- JS_Programming_Fundamentals/03_Arrays_Exercise/03_merge_arrays.js
+++ JS_Programming_Fundamentals/03_Arrays_Exercise/03_merge_arrays.js
@@ -1,1 +1,22 @@
-s
+function mergeArrays(array1, array2){
+    let newArray = []
+
+    for (let i = 0; i < array1.length; i++){
+        let elementFirstArray =array1[i]
+        let elementSecondArray = array2[i]
+        let newElement;
+
+        if (i % 2 === 0){
+            newElement = Number (elementFirstArray) + Number (elementSecondArray)
+
+        }
+        else{
+            newElement = elementFirstArray + elementSecondArray
+        }
+        newArray.push(newElement)
+    }
+    console.log(newArray.join(" - "))
+}
+mergeArrays(['5', '15', '23', '56', '35'],
+['17', '22', '87', '36', '11']
+)
