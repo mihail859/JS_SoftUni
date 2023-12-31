0ac2329440062cb4794ddb18c4af5929bd9a1fa9
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-12-31 15:12:26 +0200

    Update 06_sign_check.js

--- JS_Programming_Fundamentals/04_Functions - Lab/06_sign_check.js
+++ JS_Programming_Fundamentals/04_Functions - Lab/06_sign_check.js
@@ -1,3 +1,24 @@
 function isPositive(num1, num2, num3){
-    
+    let isPositiveBool = false
+    if (num1 > 0 & num2 > 0 & num3 > 0){
+        isPositiveBool = true
+    }
+    else if ((num1 < 0 & num2 < 0) | (num1 < 0 & num3 < 0) | (num2 < 0 & num3 < 0)){
+        isPositiveBool = true
+    }   
+    if (isPositiveBool){
+        console.log("Positive")
+    }
+    else{
+        console.log("Negative")
+    }
 }
+
+isPositive( 5,
+    12,
+   -15
+   )
+isPositive(-6,
+    -12,
+     14
+    )   
