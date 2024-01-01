159816200e0f6f9ed4de501763c53091cda1e120
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-01 23:12:05 +0200

    Update 01_sum_first_and_last.js

--- JS_Programming_Fundamentals/05_Arrays Advanced - Lab/01_sum_first_and_last.js
+++ JS_Programming_Fundamentals/05_Arrays Advanced - Lab/01_sum_first_and_last.js
@@ -1,3 +1,15 @@
 function sumFirstAndLastElement(arrayWithNumbers){
-    
+    let stringNumbers = arrayWithNumbers.map(str => parseInt(str, 10))
+    let sum;
+    if (stringNumbers.length > 1){
+        sum = stringNumbers[0] + stringNumbers[stringNumbers.length - 1]
+        
+    }else{
+        sum = arrayWithNumbers[0]
+    }
+    console.log(sum)
+
+
 }
+
+sumFirstAndLastElement(['20', '30', '40'])
