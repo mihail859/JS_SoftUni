ca236c976b8058eb94057b43defaaa3701124452
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-01 15:17:49 +0200

    Update 10_factorial_division.js

---
+++ JS_Programming_Fundamentals/04_Functions - Exercise/10_factorial_division.js
@@ -0,0 +1,20 @@
+
+function calculateFactorial(num) {
+    if (num === 0 || num === 1) {
+        return 1;
+    } else {
+        return num * calculateFactorial(num - 1);
+    }
+}
+
+function calculateAndPrintDivision(num1, num2) {
+
+    const factorialNum1 = calculateFactorial(num1);
+    const factorialNum2 = calculateFactorial(num2);
+
+    const result = factorialNum1 / factorialNum2;
+
+    console.log(`${result.toFixed(2)}`);
+}
+
+calculateAndPrintDivision(5, 2);
