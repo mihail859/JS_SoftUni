6e5e10ddbc74e8687db9e9f4b937b944b9dd4e58
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-01 23:22:31 +0200

    Update 02_negative-or-positive_numbers.js

--- JS_Programming_Fundamentals/05_Arrays Advanced - Lab/02_negative-or-positive_numbers.js
+++ JS_Programming_Fundamentals/05_Arrays Advanced - Lab/02_negative-or-positive_numbers.js
@@ -1,3 +1,16 @@
-function solve(){
-    
+function solve(arrayToSort){
+    let arrayToSort1 = arrayToSort.map(Number)
+    let result = []
+
+    for (let currNumber of arrayToSort1){
+        if (currNumber >= 0){
+            result.push(currNumber)
+        }
+        else{
+            result.unshift(currNumber)
+        }
+    }
+    console.log(result.join("\n"))
 }
+
+solve(['7', '-2', '8', '9'])
