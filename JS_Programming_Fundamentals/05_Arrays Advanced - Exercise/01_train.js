9d27fab0d0d0c69f338b94cb8a9c313e310224ac
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-03 16:41:19 +0200

    Update 01_train.js

--- JS_Programming_Fundamentals/05_Arrays Advanced - Exercise/01_train.js
+++ JS_Programming_Fundamentals/05_Arrays Advanced - Exercise/01_train.js
@@ -1,6 +1,29 @@
 function train(arrayInput){
     let wagons = arrayInput[0].split(" ").map(Number)
     let maxCapacityPerWagon = Number (arrayInput[1])
+    let people;
+
+    for (let i = 2; i < arrayInput.length; i++){
+
+        let currentData = arrayInput[i].split(" ")
+        if (currentData.length === 1){
+            people = Number (currentData[0])
+            for (let i = 0; i < wagons.length; i++){
+                if (wagons[i] + people <= maxCapacityPerWagon){
+                    wagons[i] += people
+                    break;
+                }
+            }
+
+        }
+        else{
+            people = Number (currentData[1])
+            wagons.push(people)
+        }
+    }
+    console.log(wagons.join(" "))
+
+    
 }
 
 train(['32 54 21 12 4 0 23',
