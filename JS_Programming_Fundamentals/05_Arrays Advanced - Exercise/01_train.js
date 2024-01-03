4135d7e977201735fcb454aea36ddcea2e37a94f
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-03 16:34:19 +0200

    Update 01_train.js

--- JS_Programming_Fundamentals/05_Arrays Advanced - Exercise/01_train.js
+++ JS_Programming_Fundamentals/05_Arrays Advanced - Exercise/01_train.js
@@ -1,3 +1,13 @@
 function train(arrayInput){
-    
+    let wagons = arrayInput[0].split(" ").map(Number)
+    let maxCapacityPerWagon = Number (arrayInput[1])
 }
+
+train(['32 54 21 12 4 0 23',
+'75',
+'Add 10',
+'Add 0',
+'30',
+'10',
+'75']
+)
