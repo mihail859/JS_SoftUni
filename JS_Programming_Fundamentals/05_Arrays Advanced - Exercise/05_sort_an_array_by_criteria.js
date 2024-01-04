c14f198c41dc05ca564f67533b059433bf2c7de2
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-04 21:57:48 +0200

    Update 05_sort_an_array_by_criteria.js

--- JS_Programming_Fundamentals/05_Arrays Advanced - Exercise/05_sort_an_array_by_criteria.js
+++ JS_Programming_Fundamentals/05_Arrays Advanced - Exercise/05_sort_an_array_by_criteria.js
@@ -1,5 +1,12 @@
 function sortByTwoCriteria(arrayToSort){
+    let sortedArray = arrayToSort.sort((a, b) => {
+        if (a.length !== b.length){
+            return a.length - b.length
+        }
+        return a.localeCompare(b)
+    })
 
+    console.log(sortedArray.join("\n"))
 }
 
 sortByTwoCriteria(['alpha', 'beta', 'gamma'])
