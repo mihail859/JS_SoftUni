f4aaebfe85ef8227bc1f8488cc3f693decd87ee9
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-04 20:15:52 +0200

    Update 02_distinct_array.js

--- JS_Programming_Fundamentals/05_Arrays Advanced - Exercise/02_distinct_array.js
+++ JS_Programming_Fundamentals/05_Arrays Advanced - Exercise/02_distinct_array.js
@@ -1,5 +1,7 @@
-function distinctArray(input){
-
+function distinctArray(repeatingArray){
+    let distinctArrayPrint = Array.from(new Set(repeatingArray))
+    console.log(distinctArrayPrint.join(" "))
 }
 
 distinctArray([1, 2, 3, 4])
+distinctArray([7, 8, 9, 7, 2, 3, 4, 1, 2])
