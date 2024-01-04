df6c64fc4061f63b05599e323a86fbc8147d0e93
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-04 23:04:32 +0200

    Update 06_bomb_numbers.js

--- JS_Programming_Fundamentals/05_Arrays Advanced - Exercise/06_bomb_numbers.js
+++ JS_Programming_Fundamentals/05_Arrays Advanced - Exercise/06_bomb_numbers.js
@@ -1,7 +1,34 @@
 function bombNumbers(sequence, bomb){
+    let specialNumber = bomb[0]
+    let power = bomb[1]
+    let specialNumberIdx;
 
+    let finalArray = sequence.slice(0)
+    let arrayLength = finalArray.length
+    let start;
+
+    for (let i=0; i < finalArray.length; i++){
+        if (specialNumber === finalArray[i]){
+
+            specialNumberIdx = i
+            start = i - power
+            if (start < 0){
+                start = 0
+            }
+            finalArray.splice(start, power*2 +1)
+            i=0
+        }
+    }
+    let sumNumbersInArray = finalArray.reduce((sum, a) => sum += a, 0)
+    console.log(sumNumbersInArray)
 }
 
-bombNumbers([1, 2, 2, 4, 2, 2, 2, 9],
-    [4, 2]
+  
+console.log("--//--")
+bombNumbers([1, 1, 2, 1, 1, 1, 2, 1, 1, 1],
+    [2, 1]
+    
+    
     )
+console.log("--//--")
+ 
