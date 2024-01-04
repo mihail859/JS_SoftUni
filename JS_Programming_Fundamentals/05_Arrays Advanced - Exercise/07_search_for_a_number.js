efabc48e61058f067dce887f4f8273b9a9fc4d09
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-04 23:17:44 +0200

    Update 07_search_for_a_number.js

--- JS_Programming_Fundamentals/05_Arrays Advanced - Exercise/07_search_for_a_number.js
+++ JS_Programming_Fundamentals/05_Arrays Advanced - Exercise/07_search_for_a_number.js
@@ -1,4 +1,12 @@
-function search(){
+function search(array1, array2){
+    let numbersToTake = array2[0]
+    let numbersToDelete = array2[1]
+    let searchedNumber = array2[2]
+
+    array1 = array1.slice(0, numbersToTake)
+    array1.splice(0, numbersToDelete)
+    let occurrences = array1.filter(el => el === searchedNumber).length
+    console.log(`Number ${searchedNumber} occurs ${occurrences} times.`)
 
 }
 
