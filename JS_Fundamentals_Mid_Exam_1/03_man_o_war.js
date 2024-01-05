2c89e40e5dbec28acddc4aec5a731a65c91ca016
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-05 22:32:11 +0200

    Update 03_man_o_war.js

--- JS_Fundamentals_Mid_Exam_1/03_man_o_war.js
+++ JS_Fundamentals_Mid_Exam_1/03_man_o_war.js
@@ -1,3 +1,15 @@
-function solve(){
-    
+function solve(input){
+    let pirateShip = input[0].split(">").map(Number)
+    console.log(pirateShip)
 }
+
+solve(["12>13>11>20>66",
+"12>22>33>44>55>32>18",
+"70",
+"Fire 2 11",
+"Fire 8 100",
+"Defend 3 6 11",
+"Defend 0 3 5",
+"Repair 1 33",
+"Status",
+"Retire"])
