0e86b4765ffa9cf427afa8e1ca1bd158bd1ad7f8
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-05 23:07:06 +0200

    Update 03_man_o_war.js

--- JS_Fundamentals_Mid_Exam_1/03_man_o_war.js
+++ JS_Fundamentals_Mid_Exam_1/03_man_o_war.js
@@ -3,18 +3,58 @@ function solve(input){
     let warship = input[1].split(">").map(Number)
     let maximumHealth = Number (input[2])
 
-    console.log(pirateShip)
-    console.log(warship)
-    console.log(maximumHealth)
+    function fire(warshipList, index, damage){
+        if (index >= 0 && index < warshipList.length){
+            warshipList[index] -= damage
+            if (warshipList[index] <= 0){
+                return false
+            }
+            else{
+                return warshipList
+            }
+        }
+        return warshipList
+    }
+
+
+    function defend(pirateShipList, idxStart, idxStop, damage){
+        if ((idxStart >= 0 && idxStart < pirateShipList.length) && (idxStop >= 0 && idxStop < pirateShipList.length)){
+            for (let i = startIndex; i <= endIndex; i++) {
+                pirateShipList[i] -= damage;
+                if (pirateShipList[i] <= 0) {
+                    console.log("You lost! The pirate ship has sunken.");
+                    return false;
+                }
+            }
+            return pirateShipList
+        }
+        return pirateShipList
+    }    
 
     let idx = 3
     while (true){
         let commandLine = input[idx].split(" ")
         if (commandLine[0] === "Retire"){
             break
-        }else{
-            console.log(commandLine)
         }
+        else if (commandLine[0] === "Fire"){
+            let i = Number (commandLine[1])
+            let damageGiven = Number (commandLine[2])
+            warship = fire(warship, i, damageGiven)
+            if (!warship){
+                console.log("You won! The enemy ship has sunken.")
+                break
+            }
+        }
+        else if(commandLine[0] === "Defend"){
+            let startIndex = Number (commandLine[1])
+            let endIndex = Number (commandLine[2])
+            let damageGiven = Number (commandLine[3])
+            pirateShip = defend(pirateShip, startIndex, endIndex, damageGiven)
+
+        }
+
+
         idx++;
 
     }
