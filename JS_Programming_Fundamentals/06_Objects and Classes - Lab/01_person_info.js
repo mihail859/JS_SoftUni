106d8f2116566992c535783e20353cabb4ca2c08
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-07 11:09:16 +0200

    Update 01_person_info.js

--- JS_Programming_Fundamentals/06_Objects and Classes - Lab/01_person_info.js
+++ JS_Programming_Fundamentals/06_Objects and Classes - Lab/01_person_info.js
@@ -1,3 +1,13 @@
-function solve(){
-    
+function solve(firstName, lastName, age){
+    let obj = {
+        firstName: firstName,
+        lastName: lastName,
+        age: age
+    }
+    return obj
 }
+
+solve("Peter", 
+"Pan",
+"20"
+)
