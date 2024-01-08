f0015b3330fa61f51abd80c6977a8bc01453be9f
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-08 22:51:33 +0200

    Update 05_inventory.js

---
+++ JS_Programming_Fundamentals/06_Objects and Classes - Exercise/05_inventory.js
@@ -0,0 +1,10 @@
+function registerForHeroes(inputData){
+    let heroes
+}
+
+registerForHeroes([
+    'Isacc / 25 / Apple, GravityGun',
+    'Derek / 12 / BarrelVest, DestructionSword',
+    'Hes / 1 / Desolator, Sentinel, Antara'
+    ]
+    )
