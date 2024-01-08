8b469650e6bc75207a5fd2c57e97ba9db7509651
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-08 22:11:50 +0200

    Update 04_movies.js

---
+++ JS_Programming_Fundamentals/06_Objects and Classes - Exercise/04_movies.js
@@ -0,0 +1,21 @@
+function solveTask(arr){
+    let moviesArray = []
+
+    for (let line of moviesArray){
+        if (line.includes('addMovie ')){
+
+        }
+    }
+
+}   
+solveTask([
+    'addMovie Fast and Furious',
+    'addMovie Godfather',
+    'Inception directedBy Christopher Nolan',
+    'Godfather directedBy Francis Ford Coppola',
+    'Godfather onDate 29.07.2018',
+    'Fast and Furious onDate 30.07.2018',
+    'Batman onDate 01.08.2018',
+    'Fast and Furious directedBy Rob Cohen'
+    ]
+    )
