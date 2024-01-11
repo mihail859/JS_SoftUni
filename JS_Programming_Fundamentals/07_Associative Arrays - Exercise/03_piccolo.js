a4a0ac8014758faf9c81649d1cea3fdd9d4a1abd
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-11 22:16:17 +0200

    Update 03_piccolo.js

---
+++ JS_Programming_Fundamentals/07_Associative Arrays - Exercise/03_piccolo.js
@@ -0,0 +1,15 @@
+function piccolo(data){
+
+}
+
+data(['IN, CA2844AA',
+'IN, CA1234TA',
+'OUT, CA2844AA',
+'IN, CA9999TT',
+'IN, CA2866HI',
+'OUT, CA1234TA',
+'IN, CA2844AA',
+'OUT, CA2866HI',
+'IN, CA9876HH',
+'IN, CA2822UU']
+)
