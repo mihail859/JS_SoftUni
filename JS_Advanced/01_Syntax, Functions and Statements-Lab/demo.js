6cc0f862836d90d61b4761f56f8107074e0af910
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-12 11:42:48 +0200

    update

--- JS_Advanced/01_Syntax, Functions and Statements-Lab/demo.js
+++ JS_Advanced/01_Syntax, Functions and Statements-Lab/demo.js
@@ -1,9 +1,15 @@
 function demo(){
-    let arr = [1, 2, 3, 4]
+    /*let arr = [1, 2, 3, 4]
     let [...copy] = arr
     arr.push(5)
     console.log(arr)
-    console.log(copy)
+    console.log(copy)*/
+    let obj = {}
+    if (obj){
+        console.log('1');
+    }else{
+        console.log('0');
+    }
 }
 
 demo()
