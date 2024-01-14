5e41c2cf49557d41bdfb13c32f431ce69db8f69b
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-14 17:30:13 +0200

    Update 01_match_full_name.js

--- JS_Programming_Fundamentals/09_Regular Expressions - Lab/01_match_full_name.js
+++ JS_Programming_Fundamentals/09_Regular Expressions - Lab/01_match_full_name.js
@@ -1,7 +1,13 @@
 function fullNameSearched(data){
-    const pattern = /\b[A-Z]{1}[a-z]{1,}[ ]{1}[A-Z][a-z]{1,}/gm
+    const pattern = /\b[A-Z]{1}[a-z]{1,}[ ]{1}[A-Z][a-z]{1,}/g
+    let resultArr = []
     let result = data.match(pattern)
-    console.log(result.join(" "));
+    let validName;
+    //console.log(result.join(" "));
+    while ((validName =  pattern.exec(data)) !== null){
+        resultArr.push(validName[0])
+    }   
+    console.log(resultArr)
 }
 
 fullNameSearched("Ivan Ivanov, Ivan ivanov, ivan Ivanov, IVan Ivanov, Test Testov, Ivan	Ivanov")
