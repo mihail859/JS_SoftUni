dbdbdf895aa2a0f34d6150e2954997081ed18ac9
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-18 08:55:05 +0200

    Update 11_equal_neighbors.js

--- JS_Advanced/02_Arrays and Nested Arrays - Lab/11_equal_neighbors.js
+++ JS_Advanced/02_Arrays and Nested Arrays - Lab/11_equal_neighbors.js
@@ -12,7 +12,8 @@ function findEqualNeighbors(matrix){
     return count
 }
 
-console.log(findEqualNeighbors([['2', '3', '4', '7', '0'],
+console.log(findEqualNeighbors(
+[['2', '3', '4', '7', '0'],
 ['4', '0', '5', '3', '4'],
 ['2', '3', '5', '4', '2'],
 ['9', '8', '7', '5', '4']]
