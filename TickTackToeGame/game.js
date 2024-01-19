73a0ea5c9f35d3949bb0a3e170ac6562cc5d8b2f
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-19 07:37:06 +0200

    Update game.js

--- TickTackToeGame/game.js
+++ TickTackToeGame/game.js
@@ -20,7 +20,7 @@ initializeGame();
 function initializeGame(){
     cells.forEach(cell => cell.addEventListener("click", cellClicked));
     restartBtn.addEventListener("click", restartGame);
-    statusText.textContent = `${currentPlayer}'s turn`;
+    statusText.textContent = `${currentPlayer}'s turn`
     running = true;
 }
 function cellClicked(){
