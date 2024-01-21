a4428d79ef73f101ccf52ffdbe903e503838247e
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-21 20:39:14 +0200

    Update EditElement.js

--- JS_Advanced/04_DOM Introduction - Lab/01.Edit Element/EditElement.js
+++ JS_Advanced/04_DOM Introduction - Lab/01.Edit Element/EditElement.js
@@ -1,3 +1,11 @@
-function editElement() {
-    // TODO
-}
+function editElement(ref, match, replacer) {
+    let string = ref.textContent;
+    
+    while (string.includes(match)){
+        string = string.replace(match, replacer)
+    }
+
+    ref.textContent = string
+
+
+}    
