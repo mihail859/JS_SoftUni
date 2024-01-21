1f110265180117ec5338ee6a77f9e93cb14d949f
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-21 22:07:14 +0200

    Update SumNumbers.js

--- JS_Advanced/04_DOM Introduction - Lab/03.Sum Numbers/SumNumbers.js
+++ JS_Advanced/04_DOM Introduction - Lab/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,8 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    const a = Number (document.getElementById('num1').value);
+    const b = Number (document.getElementById('num2').value);
+
+    let ref = document.getElementById('sum')
+    ref.value = a  + b
+    
 }
