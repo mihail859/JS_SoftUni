785734e32a90e09bf992b5aeb3d23495851b1971
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-21 22:38:04 +0200

    solve

--- JS_Advanced/04_DOM Introduction - Lab/05.Colorize Table/ColorizeTable.js
+++ JS_Advanced/04_DOM Introduction - Lab/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,12 @@
 function colorize() {
-    // TODO
-}
+    let rows = Array.from(document.querySelectorAll('table tr'))
+    
+    let idx = 0
+    for (let row of rows){
+        idx++;
+        if (idx % 2 === 0){
+            row.style.background = 'teal'
+        }
+    }
+
+}    
