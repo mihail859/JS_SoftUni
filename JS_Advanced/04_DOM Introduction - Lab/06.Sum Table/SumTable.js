d7578bc404755008c9c9c4eda36e0203e39f8538
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-22 10:38:12 +0200

    solve

--- JS_Advanced/04_DOM Introduction - Lab/06.Sum Table/SumTable.js
+++ JS_Advanced/04_DOM Introduction - Lab/06.Sum Table/SumTable.js
@@ -1,3 +1,11 @@
 function sumTable() {
+    let prices = document.getElementsByTagName('td')
+    let arr = Array.from(prices)
 
+    let totalSum = 0
+    for (let i = 1; i < arr.length-1; i+=2){
+        totalSum += Number (arr[i].textContent)
+    }
+    let elWithIdSum = document.getElementById("sum")
+    elWithIdSum.textContent = totalSum
 }
--- JS_Advanced/04_DOM Introduction - Lab/07.Extract Parenthesis/ExtractParenthesis.js
+++ JS_Advanced/04_DOM Introduction - Lab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,17 @@
 function extract(content) {
+    let toExtractText = document.getElementById(`${content}`)
+    let text1 = toExtractText.textContent
+    
+    let pattern = /\([A-Za-z ]+\)/gm
 
+    let result;
+    let towns = []
+    while ((result = pattern.exec(text1)) !== null){
+        
+        
+        towns.push(result[0].substring(1, result[0].length - 1))
+    }
+    
+    return towns.join("; ")
+    
 }
