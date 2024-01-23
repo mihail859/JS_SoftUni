37a32e061b21df3d445d5a695a449dc0c508a2e7
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-23 22:00:25 +0200

    Update solution.js

--- JS_Advanced/04-DOM Introduction - Exercise/02. Pascal or Camel Case/solution.js
+++ JS_Advanced/04-DOM Introduction - Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,22 @@
 function solve() {
-  //TODO...
+  const PASCAL_CASE = 'Pascal Case'
+  const CAMEL_CASE = 'Camel Case'
+
+  let textToModify = document.getElementById('text').value.toLowerCase();
+  let typeCase = document.getElementById('naming-convention').value;
+
+  if (typeCase !== PASCAL_CASE && typeCase !== CAMEL_CASE) {
+      document.getElementById('result').textContent = 'Error!'
+
+      return
+  }
+  let toArr = textToModify.split(" ")
+  let result = typeCase === PASCAL_CASE ? "": toArr[0]
+  let idx = typeCase === PASCAL_CASE ? 0 : 1
+
+  for (let i = idx; i < toArr.length; i++){
+    let word = toArr[i][0].toUpperCase() + toArr[i].slice(1, )
+    result += word
+  }
+  document.getElementById('result').textContent = result
 }
