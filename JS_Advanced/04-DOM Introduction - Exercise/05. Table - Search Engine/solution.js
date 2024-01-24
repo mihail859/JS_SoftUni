eb311ca6f96522829890089ca2fe32a75bcb4b66
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-24 13:26:40 +0200

    Update solution.js

--- JS_Advanced/04-DOM Introduction - Exercise/05. Table - Search Engine/solution.js
+++ JS_Advanced/04-DOM Introduction - Exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,20 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
+   let rows = document.querySelectorAll('tbody>tr');
 
    function onClick() {
-      //   TODO:
+      let searched = document.getElementById('searchField').value.toLowerCase()
 
-   }
+      for (let row of rows){
+
+         let current = row.textContent.toLowerCase()
+
+         if (current.includes(searched)){
+            row.setAttribute('class','select')
+         }else{
+            row.removeAttribute('class')
+         }
+      }
+
+   }   
 }
