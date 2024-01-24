ce4d667f6a1e119fcc9c58e4be71d77a1f9a1693
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-24 10:58:28 +0200

    update

--- JS_Advanced/04-DOM Introduction - Exercise/04. Search in List/search.js
+++ JS_Advanced/04-DOM Introduction - Exercise/04. Search in List/search.js
@@ -1,3 +1,25 @@
 function search() {
-   // TODO
+   let townsArr = document.querySelectorAll('#towns>li');
+   let arr = Array.from(townsArr);
+   let searchedWord = document.querySelector('#searchText').value.toLowerCase();
+   
+
+   let matchesFound = 0
+
+   for (el of arr){  
+      
+      let currentEl = el.textContent.toLowerCase()
+      if (currentEl.includes(searchedWord)){
+         matchesFound++;
+         el.style.textDecoration = 'underline';
+         el.style.fontWeight = "bold";
+      }else{
+         el.style.textDecoration = '';
+         el.style.fontWeight = "";
+      }
+      
+   }
+   
+   document.getElementById('result').textContent = `${matchesFound} matches found`
+
 }
