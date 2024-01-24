76003e5c7b8983366ba38c1602da44fbd26ff4a2
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-24 09:59:03 +0200

    Update accordion.js

--- JS_Advanced/04-DOM Introduction - Exercise/03. Accordion/accordion.js
+++ JS_Advanced/04-DOM Introduction - Exercise/03. Accordion/accordion.js
@@ -1,3 +1,16 @@
 function toggle() {
-    console.log('TODO:...');
+
+    const MORE = 'MORE';
+    const LESS = 'LESS'
+
+    let buttonSpan = document.querySelector('.button')
+    let text = document.querySelector('#extra')
+
+    buttonSpan.textContent = buttonSpan.textContent === MORE 
+    ? LESS
+    : MORE
+
+    text.style.display = text.style.display === 'none' || text.style.display === ''
+    ? 'block'
+    : 'none'
 }
