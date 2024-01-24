a936b35cba828dd8bb301aabeaf116d0f532c1bb
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-24 14:16:27 +0200

    Update solution.js

--- JS_Advanced/04-DOM Introduction - Exercise/06. Format the Text/solution.js
+++ JS_Advanced/04-DOM Introduction - Exercise/06. Format the Text/solution.js
@@ -1,3 +1,16 @@
 function solve() {
-  //TODO
+  const textAreaText = document.getElementById('input')
+  const outputArea = document.getElementById('output')
+  const sentences = textAreaText.value.split('.').filter(el=> el !== "").map(el => el + '.')
+  
+
+  let paragraphsCount = Math.ceil(sentences.length / 3)
+  
+  let result = '';
+  for (let i = 0; i < paragraphsCount; i++){
+    let paragraphText = sentences.splice(0, 3)
+
+     outputArea.innerHTML += `<p>${paragraphText}</p>`
+  }
+  
 }
