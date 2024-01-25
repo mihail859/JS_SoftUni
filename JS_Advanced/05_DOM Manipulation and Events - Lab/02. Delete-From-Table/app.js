ef776fb9c4f1826261fbe48942917f303a5b7195
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-25 22:11:59 +0200

    Update app.js

--- JS_Advanced/05_DOM Manipulation and Events - Lab/02. Delete-From-Table/app.js
+++ JS_Advanced/05_DOM Manipulation and Events - Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,22 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const result = document.getElementById('result');
+    const inputEmail = document.getElementsByName("email")[0].value
+    const rows = document.querySelectorAll('tbody tr');
+    const rowsAsArray = Array.from(rows)
+
+    let isRemoved  = false
+
+    rowsAsArray.forEach(element => {
+        let tdNode = element.querySelectorAll('td')[1].textContent;
+        
+        if (inputEmail === tdNode){
+            element.remove()
+            isRemoved = true
+            inputEmail.value = ""
+        }
+
+    });
+    result.textContent = isRemoved === false ? "Not found." : 'Deleted.'
+
+
 }
