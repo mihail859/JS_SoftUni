dff78e2e71f74c6b0457c33ac2ff84cb26a242b6
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-25 10:04:22 +0200

    update

--- JS_Advanced/04-DOM Introduction - Exercise/08. Generate Report/report.js
+++ JS_Advanced/04-DOM Introduction - Exercise/08. Generate Report/report.js
@@ -1,3 +1,25 @@
 function generateReport() {
-    //TODO
+    const output = document.getElementById("output")
+
+    const selectedBoxes = document.querySelectorAll('[type="checkbox"]')
+    const arrayFromNodeWithCheckBoxes = Array.from(selectedBoxes);
+
+    const result = []
+
+    const tableRows = document.querySelectorAll('tbody tr');
+    const arrayFromNodeWithTableRows = Array.from(tableRows)
+    
+
+    arrayFromNodeWithTableRows.forEach( row => {
+        let current = {}
+        Array.from(row.querySelectorAll('td')).forEach((x, i)=> {
+            if (arrayFromNodeWithCheckBoxes[i].checked){
+                current[arrayFromNodeWithCheckBoxes[i].name] = x.textContent
+            }
+        })
+        result.push(current)
+    })
+        
+    output.textContent = JSON.stringify(result)
+    
 }
