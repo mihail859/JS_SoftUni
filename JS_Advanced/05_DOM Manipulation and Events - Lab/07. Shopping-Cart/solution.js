e5b691510c187ec22286c6a0653f796688e93a27
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-26 17:33:56 +0200

    update

--- JS_Advanced/05_DOM Manipulation and Events - Lab/07. Shopping-Cart/solution.js
+++ JS_Advanced/05_DOM Manipulation and Events - Lab/07. Shopping-Cart/solution.js
@@ -1,3 +1,34 @@
 function solve() {
-   //TODO...
+   let textAreaProducts = document.querySelector('textarea')
+   let buttons = document.querySelectorAll('.add-product');
+   let arrFromButtons = Array.from(buttons);
+
+   let products = [];
+   let totalPrice = 0;
+   
+
+
+   arrFromButtons.forEach(currBtn => {
+      currBtn.addEventListener('click', addToList);
+   });
+
+   function addToList(event){
+      let productName = event.target.parentNode.parentNode.querySelector('.product-details>.product-title').textContent;
+      let price = event.target.parentNode.parentNode.querySelector('.product-line-price').textContent;
+     
+      
+      if (!products.includes(productName)){
+         products.push(productName)
+      }  ;
+
+      totalPrice += Number (price);
+      textAreaProducts.textContent += `Added ${productName} for ${price} to the cart.\n`
+   };
+   document.querySelector('.checkout').addEventListener('click', function (){
+      textAreaProducts.textContent += `You bought ${products.join(', ')} for ${totalPrice.toFixed(2)}.`
+      arrFromButtons.forEach(currBtn => {
+         currBtn.removeEventListener('click', addToList);
+      });
+   });
+   
 }
