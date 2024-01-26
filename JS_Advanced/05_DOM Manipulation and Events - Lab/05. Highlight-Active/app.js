4bccb17e2245864a53630cc361659c8ce23d4f8d
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-26 14:14:51 +0200

    Update app.js

--- JS_Advanced/05_DOM Manipulation and Events - Lab/05. Highlight-Active/app.js
+++ JS_Advanced/05_DOM Manipulation and Events - Lab/05. Highlight-Active/app.js
@@ -1,3 +1,15 @@
 function focused() {
-    console.log('TODO:...');
+    let elements = document.querySelectorAll('input[type="text"]');
+
+    let arrElements = Array.from(elements);
+
+    elements.forEach(el =>{
+        el.addEventListener('focus', function (){
+            el.parentNode.classList.add('focused');
+        });
+        el.addEventListener('blur', function (){
+            el.parentNode.classList.remove('focused')
+        })
+    })
+
 }
