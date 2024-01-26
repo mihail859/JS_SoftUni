fa64b5163df2c975e06faf2d08a9769556dc63b4
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-26 10:26:08 +0200

    Update app.js

--- JS_Advanced/05_DOM Manipulation and Events - Lab/03. Add-Delete/app.js
+++ JS_Advanced/05_DOM Manipulation and Events - Lab/03. Add-Delete/app.js
@@ -1,3 +1,27 @@
 function addItem() {
-    //TODO...
+    const ulList  = document.getElementById('items');
+    const inputField = document.getElementById('newItemText');
+
+    if (inputField.value === ""){
+        return 
+    }
+
+    const newElement = document.createElement('li');
+    newElement.textContent = inputField.value;
+    ulList.appendChild(newElement);
+
+    let deleted = document.createElement('a');
+    deleted.href = '#';
+    deleted.textContent = '[Delete]';
+    newElement.appendChild(deleted);
+
+    deleted.addEventListener('click', remove);
+
+    function remove(){
+        newElement.remove()
+    }
+
+
+    inputField.value = '';
+
 }
