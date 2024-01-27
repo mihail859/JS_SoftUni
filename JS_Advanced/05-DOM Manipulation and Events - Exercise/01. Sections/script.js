4a672e606db6a2914ee9223a3675151ef71984b7
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-27 17:13:16 +0200

    Update script.js

--- JS_Advanced/05-DOM Manipulation and Events - Exercise/01. Sections/script.js
+++ JS_Advanced/05-DOM Manipulation and Events - Exercise/01. Sections/script.js
@@ -1,3 +1,16 @@
 function create(words) {
-   console.log('TODO:...');
+   const parentElement = document.getElementById('content')
+   words.forEach(element => {
+      let newDivElement = document.createElement('div')
+      let pInDiv = document.createElement('p')
+      pInDiv.style.display = 'none'
+      pInDiv.textContent = element
+
+      newDivElement.appendChild(pInDiv)
+      newDivElement.addEventListener('click', function (event){
+         event.target.querySelector('p').style.display = 'block'
+      })
+
+      parentElement.appendChild(newDivElement)
+   });
 }
