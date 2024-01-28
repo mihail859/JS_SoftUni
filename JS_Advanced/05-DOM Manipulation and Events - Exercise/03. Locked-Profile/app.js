fb80e0a57ea39de9ffb76847e46d88c76bea4eac
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-28 16:24:46 +0200

    Update app.js

--- JS_Advanced/05-DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
+++ JS_Advanced/05-DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,24 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const buttonsNode = document.querySelectorAll('div button');
+    const buttonsArr = Array.from(buttonsNode)
+    buttonsArr.forEach(button => {
+        button.addEventListener('click', showInformation)
+    });
+
+    function showInformation(event) {
+        const SHOW_MORE = 'Show more'
+        const HIDE_IT = 'Hide it'
+        const DISPLAY_BLOCK = 'block'
+        const DISPLAY_NONE = 'none'
+
+        const parent = event.target.parentElement;
+
+        let radioButtonUnlock = parent.querySelector('input[value="unlock"]')
+
+        if (radioButtonUnlock.checked) {
+            let hiddenDiv = parent.querySelector('div')
+            hiddenDiv.style.display === DISPLAY_BLOCK ? hiddenDiv.style.display = DISPLAY_NONE : hiddenDiv.style.display = DISPLAY_BLOCK
+            event.target.textContent === SHOW_MORE ?  event.target.textContent = HIDE_IT : event.target.textContent = SHOW_MORE
+        }
+    };
 }
