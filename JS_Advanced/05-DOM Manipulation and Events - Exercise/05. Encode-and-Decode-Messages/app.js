d1deec2adc7af5a60e78f4af04e605aa221b78b1
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-29 06:21:10 +0200

    Update app.js

--- JS_Advanced/05-DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/app.js
+++ JS_Advanced/05-DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,30 @@
 function encodeAndDecodeMessages() {
+    const textAreaMessage = document.querySelector('textarea:first-of-type');
+    const textAreaLastReceived = document.querySelector('#main>div:last-of-type>textarea');
+    const encodeBtn = document.querySelector('button:first-of-type');
+    const decodeBtn = document.querySelector('#main>div:last-of-type>button')
     
+    encodeBtn.addEventListener('click', encodeFunction);
+    decodeBtn.addEventListener('click', decodeFunction)
+
+    function encodeFunction(){
+        let allText = [...textAreaMessage.value]
+
+        allText = allText.map((el ,i, self) => {
+             return  String.fromCharCode(el.charCodeAt(0) + 1) 
+        });
+        
+        let encodedMessage = allText.join("");
+        textAreaLastReceived.textContent = encodedMessage
+        textAreaMessage.value = ""
+
+    }
+
+    function decodeFunction(){
+        let textToDecode =  [...textAreaLastReceived.textContent]
+        textToDecode = textToDecode.map(el => String.fromCharCode(el.charCodeAt(0) - 1));
+
+        let decodedMessage = textToDecode.join("");
+        textAreaLastReceived.textContent = decodedMessage
+    }
 }
