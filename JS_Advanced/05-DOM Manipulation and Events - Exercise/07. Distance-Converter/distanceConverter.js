71896458c8943048b21465c003659c5b45e6dddd
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-29 22:00:03 +0200

    Update distanceConverter.js

--- JS_Advanced/05-DOM Manipulation and Events - Exercise/07. Distance-Converter/distanceConverter.js
+++ JS_Advanced/05-DOM Manipulation and Events - Exercise/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,14 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    const inputUnits = document.getElementById('inputUnits');
+    const convertBtn = document.getElementById('convert');
+    const outputUnits = document.getElementById('outputUnits');
+    convertBtn.addEventListener('click', convert)
+
+    function convert(){
+        let inputNumber = document.getElementById('inputDistance').value ;
+        let selectUnit = inputUnits.options[inputUnits.selectedIndex].value
+        let outputUnit = outputUnits.options[outputUnits.selectedIndex].value
+        
+        
+    }
 }
