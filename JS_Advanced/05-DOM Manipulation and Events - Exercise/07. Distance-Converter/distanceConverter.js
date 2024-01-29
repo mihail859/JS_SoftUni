90f16fee9c91202a22cadf168605e3326f0596d2
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-29 23:41:05 +0200

    Update distanceConverter.js

--- JS_Advanced/05-DOM Manipulation and Events - Exercise/07. Distance-Converter/distanceConverter.js
+++ JS_Advanced/05-DOM Manipulation and Events - Exercise/07. Distance-Converter/distanceConverter.js
@@ -2,13 +2,30 @@ function attachEventsListeners() {
     const inputUnits = document.getElementById('inputUnits');
     const convertBtn = document.getElementById('convert');
     const outputUnits = document.getElementById('outputUnits');
-    convertBtn.addEventListener('click', convert)
+    const outputField = document.getElementById('outputDistance');
+
+    convertBtn.addEventListener('click', convert);
+
+    let convertObj = {
+        "km"  : 1000,
+        "m"   : 1,
+        "cm"  : 0.01,
+        "mm"  : 0.001,
+        "mi"  : 1609.34,
+        "yrd" : 0.9144,
+        "ft"  : 0.3048,
+        "in"  : 0.0254
+    }
 
     function convert(){
-        let inputNumber = document.getElementById('inputDistance').value ;
+        let inputNumber = Number (document.getElementById('inputDistance').value) ;
         let selectUnit = inputUnits.options[inputUnits.selectedIndex].value
         let outputUnit = outputUnits.options[outputUnits.selectedIndex].value
         
+        let inMeters = convertObj[selectUnit] * inputNumber
+        let output = inMeters / convertObj[outputUnit]
+
+        outputField.value = output
         
     }
 }
