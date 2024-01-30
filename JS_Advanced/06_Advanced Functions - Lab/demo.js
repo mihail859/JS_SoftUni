ddd4fa1cbcf9281f02ddfd7fd790f6fc66d029a5
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-01-30 23:50:13 +0200

    Update demo.js

--- JS_Advanced/06_Advanced Functions - Lab/demo.js
+++ JS_Advanced/06_Advanced Functions - Lab/demo.js
@@ -1,10 +1,15 @@
-function sayHello(){
-    console.log(this.name + ' ' + 'said hello')
+function sayHello(grade){
+    console.log(this.name + ' ' + 'said hello');
+    console.log(this.age  + '`s ' + 'old student had a ' + grade)
 }
 
 const obj = {
-    name: 'Georgi'
+    name: 'Georgi',
+    age: 21
 }
 
-sayHello.call(obj)
-sayHello.apply(obj)
+sayHello.call(obj, 8)
+sayHello.apply(obj, [5]);
+
+let bindFunction = sayHello.bind(obj, 8);
+bindFunction()
