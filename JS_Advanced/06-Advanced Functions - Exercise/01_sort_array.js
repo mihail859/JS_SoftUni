c9c0716c5e01fbc35613d26aa0585f797eec9027
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-02-02 21:38:33 +0200

    Update 01_sort_array.js

--- JS_Advanced/06-Advanced Functions - Exercise/01_sort_array.js
+++ JS_Advanced/06-Advanced Functions - Exercise/01_sort_array.js
@@ -1,1 +1,10 @@
-a
+function sortArrByCriteria(arr, criteria){
+    const criteriaObj = {
+        asc: (a, b) => a-b,
+        desc: (a,b) => b-a 
+    } 
+    return arr.sort(criteriaObj[criteria])
+}
+
+console.log(sortArrByCriteria([14, 7, 17, 6, 8], 'asc'))
+console.log(sortArrByCriteria([14, 7, 17, 6, 8], 'desc'))
