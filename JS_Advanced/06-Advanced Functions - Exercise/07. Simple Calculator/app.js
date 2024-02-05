48255b49d8dd736f425e1ed2db013ee41e20d112
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-02-05 10:47:42 +0200

    Update app.js

--- JS_Advanced/06-Advanced Functions - Exercise/07. Simple Calculator/app.js
+++ JS_Advanced/06-Advanced Functions - Exercise/07. Simple Calculator/app.js
@@ -1,6 +1,32 @@
 function calculator() {
-    // TODO:
+    
+    const htmlElements = {
+        'element1': "",
+        'element2': "",
+        'result': ""
+    };
+
+    function init(selector1, selector2, resultSelector){
+        htmlElements.element1 = document.querySelector(selector1)
+        htmlElements.element2 = document.querySelector(selector2)
+        htmlElements.result = document.querySelector(resultSelector)
+    }
+    function add(){
+        htmlElements.result.value = Number(htmlElements.element1.value) + Number(htmlElements.element2.value)
+    }
+    function subtract (){
+        htmlElements.result.value = Number(htmlElements.element1.value) - Number(htmlElements.element2.value)
+    }
+
+    return {
+        init,
+        add,
+        subtract
+    }
 }
+
+const calculate = calculator (); 
+calculate.init ('#num1', '#num2', '#result'); 
 
 
 
