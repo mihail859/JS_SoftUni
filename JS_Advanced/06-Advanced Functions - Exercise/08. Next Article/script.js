8fd5e7ed73e2c99f3d40a5181505fabe8c957d4b
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-02-05 10:59:08 +0200

    Update script.js

--- JS_Advanced/06-Advanced Functions - Exercise/08. Next Article/script.js
+++ JS_Advanced/06-Advanced Functions - Exercise/08. Next Article/script.js
@@ -1,3 +1,14 @@
 function getArticleGenerator(articles) {
-    // TODO
+    const divContainer = document.getElementById('content')
+    let copyOfArticles = articles.slice(0)
+
+    return function showNext () {
+        if (copyOfArticles.length >=1){
+            let newArticle = document.createElement('article')
+            newArticle.innerHTML = copyOfArticles.shift()
+            divContainer.appendChild(newArticle)
+        }
+        return showNext
+    }
+
 }
