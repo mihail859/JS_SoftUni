d91d5ef267729feb84ac4dd8721a6dc6de2902d3
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-02-10 21:43:06 +0200

    Update 01_sub_sum.js

--- JS_Advanced/07_Unit Testing and Error Handling - Lab/01_sub_sum.js
+++ JS_Advanced/07_Unit Testing and Error Handling - Lab/01_sub_sum.js
@@ -1,8 +1,16 @@
 function sumElementsInRange(arr, start_index, end_index){
-    if (Array.isArray(arr)){
+    if (!Array.isArray(arr)){
         return NaN;
     }
+    const startIdx = start_index < 0 ? 0 : start_index
+    const lastIdx = end_index > arr.length -1 ? arr.length - 1 : end_index;
     
+    
+
+    return arr.slice(startIdx, lastIdx + 1)
+       .map(Number)
+       .reduce((acc, num) => acc + num, 0)
+
 };
 
 console.log(sumElementsInRange([10, 20, 30, 40, 50, 60], 3, 300));
