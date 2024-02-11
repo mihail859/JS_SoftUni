3b8d429538cb183501ffef722c817150ae8b4d0d
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-02-11 12:06:40 +0200

    update

--- JS_Advanced/07_Unit Testing and Error Handling - Lab/Unit_Tests/test/my-test.test.js
+++ JS_Advanced/07_Unit Testing and Error Handling - Lab/Unit_Tests/test/my-test.test.js
@@ -1,28 +1,8 @@
-import { expect } from 'chai';
+import {assert} from "chai"
 
-const { sum, product } = require("../utils/my_math")
 
-describe('sum', ()=>{
-    it('should return correct result with array of numbers', ()=>{
-        // Arrange
-        let array = [1, 2, 3];
-
-        // Act
-        let result = sum(array);
-
-        // Assert
-        expect(result).to.be.equal(6);
-    });
-    it('should return NaN if input is not an array', ()=>{
-
-    });
-    it('should calculate result with array of mixed types', ()=>{
-
-    });
-});
-
-describe('product', ()=>{
-    it('should return correct result', ()=>{
-
-    });
+describe("my first test", ()=>{
+    it("test", ()=>{
+        assert.isOk(true)
+    })
 })
