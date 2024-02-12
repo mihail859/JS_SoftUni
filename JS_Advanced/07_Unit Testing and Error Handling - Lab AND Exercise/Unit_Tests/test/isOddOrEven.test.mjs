b1e55d8f33b8f03f1fc6be0a66bcb1e39bcf8e4e
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-02-12 12:28:40 +0200

    commit

--- JS_Advanced/07_Unit Testing and Error Handling - Lab AND Exercise/Unit_Tests/test/isOddOrEven.test.mjs
+++ JS_Advanced/07_Unit Testing and Error Handling - Lab AND Exercise/Unit_Tests/test/isOddOrEven.test.mjs
@@ -1,10 +1,14 @@
-import {assert} from 'chai';
+import { expect } from "chai";
+import isOddOrEven from "../02_task/isOddOrEven.mjs";
 
-
-import  isOddOrEven from '../02_task/isOddOrEven.mjs'
-
-describe("isOddOrEven", ()=>{
-    it('test with non string value', ()=>{
-        
+describe('isOddOrEven', ()=>{
+    it('isString', ()=>{
+        expect(isOddOrEven(3)).to.undefined
+    });
+    it('isEven', ()=>{
+        expect(isOddOrEven('aa')).to.equal('even')
     })
-})
+    it('isOdd', ()=>{
+        expect(isOddOrEven('a')).to.equal('odd')
+    })
+});
