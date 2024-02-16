132ee2f283a04de017c57902e9df2fb8fa11e499
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-02-16 11:16:04 +0200

    Update 02_String_Extension.js

--- JS_Advanced/09-Prototypes and Inheritance - Exercise/02_String_Extension.js
+++ JS_Advanced/09-Prototypes and Inheritance - Exercise/02_String_Extension.js
@@ -17,17 +17,65 @@
     String.prototype.isEmpty = function (){
         return this.toString().length == 0
     };
-    String.prototype.truncate = function (){
-        
+    String.prototype.truncate = function (n){
+        let strValue = this.toString();
+        const ELLIPSIS = "..."
+        const SINGLE_DOT = "."
+
+        if (strValue.length <= n){
+            return strValue;
+        };
+        if (n < 4){
+            return SINGLE_DOT.repeat(n)
+        };
+        const truncValue = strValue.substr(0, n - 2);
+        const lastIndex = truncValue.lastIndexOf(" ");
+
+    if (lastIndex !== -1) {
+      return `${strValue.substr(0, lastIndex)}${ELLIPSIS}`;
+    }
+
+    return `${strValue.substr(0, n - 3)}${ELLIPSIS}`;
+
+    }
+
+    String.format = function(string, ...params){
+        let str = string;
+        for (let i=0; i < params.length; i++){
+            let idx = str.indexOf(`{${i}}`)
+            while (idx !== -1){
+                str = str.replace(`{${i}}`, params[i])
+                idx = str.indexOf(`{${i}}`)
+            }
+        }   
+        return str;
     }
 })();
 
-let str = 'my string';
-str = str.ensureStart('my');
+let str = "my string";
+str = str.ensureStart("my");
 console.log(str);
 
-str = str.ensureStart('hello ');
+str = str.ensureStart("hello ");
 console.log(str);
 
-let newStr = "wer"
-console.log(newStr.isEmpty())
+str = str.truncate(16);
+console.log(str);
+
+str = str.truncate(14);
+console.log(str);
+
+str = str.truncate(8);
+console.log(str);
+
+str = str.truncate(4);
+console.log(str);
+
+str = str.truncate(2);
+console.log(str);
+
+str = String.format("The {0} {1} fox", "quick", "brown");
+console.log(str);
+
+str = String.format("jumps {0} {1}", "dog");
+console.log(str);
