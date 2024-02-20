25dfd0aef5eb2f81c1cee9944860641d6442ba22
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-02-20 20:38:29 +0200

    Update 02.Fashion Retail Management.js

--- JS_ADVANCED_EXAM_PREP/02.Fashion Retail Management.js
+++ JS_ADVANCED_EXAM_PREP/02.Fashion Retail Management.js
@@ -5,6 +5,21 @@ class FashionRetailInventory {
         this.productStock = []
     }
     addProduct (productName, size, quantity, price){
+        let productFind = this.productStock.find(p => p.productName === productName && p.size === size)
+        if (productFind){
+            productFind.quantity += quantity
+            return `You added ${quantity} more pieces of product ${productName} size ${size}`
+        }else{
+            this.productStock.push({productName, size, quantity, price})
+        }
+    }
+    sendProduct (productName, size) {
+        let productFind = this.productStock.find(p => p.productName === productName && p.size === size)
         
+        if (!productFind){
+            throw new Error (`The product ${productName}, size ${size} is not in the inventory`)
+        }
+        let idxToRemove = this.productStock.indexOf(productFind)
+        this.productStock.splice(idxToRemove, 1)
     }
 }
