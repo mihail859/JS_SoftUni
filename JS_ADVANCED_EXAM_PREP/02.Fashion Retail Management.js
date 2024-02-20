5574c59dd898177e3b4ee3bc3550ebae0af0cf18
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2024-02-20 20:27:24 +0200

    Update 02.Fashion Retail Management.js

--- JS_ADVANCED_EXAM_PREP/02.Fashion Retail Management.js
+++ JS_ADVANCED_EXAM_PREP/02.Fashion Retail Management.js
@@ -1,3 +1,10 @@
 class FashionRetailInventory {
-    
+    constructor(storehouse){
+        this.storehouse = storehouse
+        this.location = ""
+        this.productStock = []
+    }
+    addProduct (productName, size, quantity, price){
+        
+    }
 }
